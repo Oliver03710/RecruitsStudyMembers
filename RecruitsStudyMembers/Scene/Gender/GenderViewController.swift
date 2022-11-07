ba9a6e4cd2e86e7e25e74833cf832446336dde27
Oliver03710 <jhee2619@gmail.com>
2022-11-08 01:15:20 +0900

    Create & Set Gender View Controller & View

---
+++ RecruitsStudyMembers/Scene/Gender/GenderViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GenderViewController.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/08.
+//
+
+import UIKit
+
+final class GenderViewController: BaseViewController {
+
+    // MARK: - Properties
+    
+    let genderView = GenderView()
+    
+    
+    // MARK: - Init
+    
+    override func loadView() {
+        view = genderView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
