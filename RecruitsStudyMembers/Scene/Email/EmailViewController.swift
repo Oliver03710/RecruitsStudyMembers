f7d695aed656de2fd0f0ae6ef42cc565fe47df1e
Oliver03710 <jhee2619@gmail.com>
2022-11-08 01:13:56 +0900

    Create & Set Email View Controller & View

---
+++ RecruitsStudyMembers/Scene/Email/EmailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  EmailViewController.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/08.
+//
+
+import UIKit
+
+final class EmailViewController: BaseViewController {
+
+    // MARK: - Properties
+    
+    let emailView = EmailView()
+    
+    
+    // MARK: - Init
+    
+    override func loadView() {
+        view = emailView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
