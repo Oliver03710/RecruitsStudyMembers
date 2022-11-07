03b85f758119b597892a17dc1225e73129b7b80b
Oliver03710 <jhee2619@gmail.com>
2022-11-08 00:51:14 +0900

    Create Base View

---
+++ RecruitsStudyMembers/Utilities/BaseComponents/BaseView.swift
@@ -0,0 +1,24 @@
+//
+//  BaseView.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/08.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() { }
+    func setConstraints() { }
+}
