3ebc55cf5097a3e46750de402b6ad5a837207029
Oliver03710 <jhee2619@gmail.com>
2022-11-08 01:12:49 +0900

    Create & Set Birth View Controller & View

---
+++ RecruitsStudyMembers/Scene/Birth/BirthView.swift
@@ -0,0 +1,40 @@
+//
+//  BirthView.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/08.
+//
+
+import UIKit
+
+final class BirthView: BaseView {
+
+    // MARK: - Properties
+    
+    
+    
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    override func configureUI() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+
+}
---
+++ RecruitsStudyMembers/Scene/Birth/BirthViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BirthViewController.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/08.
+//
+
+import UIKit
+
+final class BirthViewController: BaseViewController {
+
+    // MARK: - Properties
+    
+    let birthView = BirthView()
+    
+    
+    // MARK: - Init
+    
+    override func loadView() {
+        view = birthView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
