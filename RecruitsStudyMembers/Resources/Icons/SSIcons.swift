2f5ae9abf3e8ac545bd69fd025f705cddb0188ec
Oliver03710 <jhee2619@gmail.com>
2022-11-08 00:38:53 +0900

    Add Splash View Assets & Create Enum For Splash Assets & Create A Folder For Managing Images

--- RecruitsStudyMembers/Resources/Icons/SSIcons.swift
+++ RecruitsStudyMembers/Resources/Icons/SSIcons.swift
@@ -48,25 +48,3 @@ enum TapIcons: String {
     case friendsAct, friendsInact
     case myAct, myInact
 }
-
-enum BackgroundImages: Int {
-    case sesacBackground1 = 1
-    case sesacBackground2
-    case sesacBackground3
-    case sesacBackground4
-    case sesacBackground5
-    case sesacBackground6
-    case sesacBackground7
-    case sesacBackground8
-    case sesacBackground9
-}
-
-enum faceImages: Int {
-    case sessacFace1 = 1
-    case sessacFace2
-    case sessacFace3
-    case sessacFace4
-    case sessacFace5
-    
-}
-
---
+++ RecruitsStudyMembers/Resources/Images/SSImages.swift
@@ -0,0 +1,33 @@
+//
+//  SSImages.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/08.
+//
+
+import Foundation
+
+enum BackgroundImages: Int {
+    case sesacBackground1 = 1
+    case sesacBackground2
+    case sesacBackground3
+    case sesacBackground4
+    case sesacBackground5
+    case sesacBackground6
+    case sesacBackground7
+    case sesacBackground8
+    case sesacBackground9
+}
+
+enum FaceImages: Int {
+    case sessacFace1 = 1
+    case sessacFace2
+    case sessacFace3
+    case sessacFace4
+    case sessacFace5
+}
+
+enum SplashImages {
+    case splashLogo
+    case splashText
+}
