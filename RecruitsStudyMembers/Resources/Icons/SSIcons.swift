fd46866e7f329bc12935430f98c6c71dfae9fe09
Oliver03710 <jhee2619@gmail.com>
2022-11-08 00:18:39 +0900

    Add More Icons & Images & Set Enum

---
+++ RecruitsStudyMembers/Resources/Icons/SSIcons.swift
@@ -0,0 +1,72 @@
+//
+//  SSIcons.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/07.
+//
+
+import Foundation
+
+enum GeneralIcons: String {
+    case antenna
+    case arrow
+    case badge
+    case bell
+    case cancelMatch
+    case check
+    case closeBig
+    case closeSmall
+    case faq
+    case filterControl
+    case friendsAct
+    case friendsInact
+    case friendsPlus
+    case graySprout
+    case logout
+    case man
+    case mapMarker
+    case message
+    case more
+    case moreArrow
+    case notice
+    case permit
+    case place
+    case plus
+    case qna
+    case search
+    case sendAct
+    case sendInact
+    case settingAlarm
+    case siren
+    case woman
+    case write
+}
+
+enum TapIcons: String {
+    case homeAct, homeInact
+    case shopAct, shopInact
+    case friendsAct, friendsInact
+    case myAct, myInact
+}
+
+enum BackgroundImages: Int {
+    case sesacBackground1 = 1
+    case sesacBackground2
+    case sesacBackground3
+    case sesacBackground4
+    case sesacBackground5
+    case sesacBackground6
+    case sesacBackground7
+    case sesacBackground8
+    case sesacBackground9
+}
+
+enum faceImages: Int {
+    case sessacFace1 = 1
+    case sessacFace2
+    case sessacFace3
+    case sessacFace4
+    case sessacFace5
+    
+}
+
