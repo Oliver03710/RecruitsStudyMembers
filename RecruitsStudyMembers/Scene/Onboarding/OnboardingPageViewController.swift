840edbf818ce42feb0e2ef0685104e243fed4800
Oliver03710 <jhee2619@gmail.com>
2022-11-08 18:08:12 +0900

    Set Components In Page View Controller

--- RecruitsStudyMembers/Scene/Onboarding/OnboardingPageViewController.swift
+++ RecruitsStudyMembers/Scene/Onboarding/OnboardingPageViewController.swift
@@ -7,23 +7,100 @@
 
 import UIKit
 
-class OnboardingPageViewController: UIPageViewController {
+final class OnboardingPageViewController: UIPageViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    
+    var pages: [UIViewController] = [FirstViewController(), SecondViewController(), ThirdViewController()]
+    
+    
+    // MARK: - Init
+    
+    override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey : Any]? = nil) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    func configureUI() {
+        PageVCDelegate()
+        initViewControllers()
+    }
+    
+    func PageVCDelegate() {
+        dataSource = self
+        delegate = nil
+    }
+    
+    func initViewControllers() {
+        setViewControllers([pages[0]], direction: .forward, animated: false, completion: nil)
+    }
 
 }
+
+
+// MARK: - Extension: UIPageViewControllerDelegate
+
+extension OnboardingPageViewController: UIPageViewControllerDelegate {
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        
+        guard let firstVC = pageViewController.viewControllers?.first else {
+            return 0
+        }
+        guard let firstVCIndex = pages.firstIndex(of: firstVC) else {
+            return 0
+        }
+        
+        return firstVCIndex
+    }
+    
+}
+
+
+// MARK: - Extenstion: UIPageViewControllerDataSource
+
+extension OnboardingPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else { return pages.last }
+        
+        guard pages.count > previousIndex else { return nil }
+        
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < pages.count else { return pages.first }
+        
+        guard pages.count > nextIndex else { return nil }
+        
+        return pages[nextIndex]
+    }
+    
+}
+
