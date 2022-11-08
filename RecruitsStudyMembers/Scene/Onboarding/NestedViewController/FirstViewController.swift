b465ab06d2f0bfa6732b4348cfb8a77f03be79b8
Oliver03710 <jhee2619@gmail.com>
2022-11-08 17:10:50 +0900

    Create View Controller Components For Page View Contollers

---
+++ RecruitsStudyMembers/Scene/Onboarding/NestedViewController/FirstViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/08.
+//
+
+import UIKit
+
+final class FirstViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
