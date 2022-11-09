fb6d63f5ac96c1661d59623b9009ff15538129ab
Oliver03710 <jhee2619@gmail.com>
2022-11-09 15:40:01 +0900

    Add Objects On Login View

--- RecruitsStudyMembers/Scene/Login/LoginView.swift
+++ RecruitsStudyMembers/Scene/Login/LoginView.swift
@@ -7,23 +7,38 @@
 
 import UIKit
 
+import SnapKit
+
 final class LoginView: BaseView {
 
     // MARK: - Properties
     
+    let instructionLabel: SignUpLabel = {
+        let label = SignUpLabel(text: "새싹 서비스 이용을 위해 휴대폰 번호를 입력해 주세요", textFont: SSFonts.display1R20.fonts, size: SSFonts.display1R20.size, lineHeight: SSFonts.display1R20.lineHeight)
+        return label
+    }()
     
+    let phoneNumTextField: SignupTextField = {
+        let tf = SignupTextField(placeHolder: "휴대폰 번호(-없이 숫자만 입력)")
+        return tf
+    }()
+    
+    let lineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = SSColors.gray6.color
+        return view
+    }()
+    
+    let getCertiNumButton: CustomButton = {
+        let btn = CustomButton(text: "", buttonColor: SSColors.gray6.color)
+        return btn
+    }()
     
     
     // MARK: - Init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configureUI()
-        setConstraints()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     
