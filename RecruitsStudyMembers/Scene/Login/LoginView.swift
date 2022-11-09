ed02a0481781465a8cff0d0792c0e7edfdba73b3
Oliver03710 <jhee2619@gmail.com>
2022-11-10 01:54:42 +0900

    Set Bind Data

--- RecruitsStudyMembers/Scene/Login/LoginView.swift
+++ RecruitsStudyMembers/Scene/Login/LoginView.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
 import SnapKit
 
 final class LoginView: BaseView {
@@ -20,6 +22,7 @@ final class LoginView: BaseView {
     
     let phoneNumTextField: SignupTextField = {
         let tf = SignupTextField(placeHolder: "휴대폰 번호(-없이 숫자만 입력)")
+        tf.keyboardType = .decimalPad
         return tf
     }()
     
@@ -31,8 +34,11 @@ final class LoginView: BaseView {
     
     let getCertiNumButton: CustomButton = {
         let btn = CustomButton(text: "인증 문자 받기", buttonColor: SSColors.gray6.color)
+        btn.isEnabled = false
         return btn
     }()
+    
+    let viewModel = LoginViewModel()
     
     
     // MARK: - Init
@@ -45,7 +51,7 @@ final class LoginView: BaseView {
     // MARK: - Helper Functions
     
     override func configureUI() {
-        
+        bindData()
     }
     
     override func setConstraints() {
@@ -75,7 +81,36 @@ final class LoginView: BaseView {
             make.bottom.equalTo(phoneNumTextField.snp.top).dividedBy(1.35)
             make.centerX.equalTo(self.snp.centerX)
         }
+    }
+    
+    func bindData() {
+        let input = LoginViewModel.Input(textFieldText: phoneNumTextField.rx.text, textFieldIsEditing: phoneNumTextField.rx.controlEvent([.editingChanged]))
+        let output = viewModel.transform(input: input)
         
+        output.phoneNum
+            .withUnretained(self)
+            .bind { (vc, bool) in
+                vc.getCertiNumButton.isEnabled = bool
+                vc.getCertiNumButton.backgroundColor = bool ? SSColors.green.color : SSColors.gray6.color
+            }
+            .disposed(by: viewModel.disposeBag)
+        
+        output.textChanged
+            .bind(to: phoneNumTextField.rx.text)
+            .disposed(by: viewModel.disposeBag)
+        
+        output.isEditing
+            .withUnretained(self)
+            .bind { (vc, bool) in
+                vc.lineView.backgroundColor = bool ? SSColors.black.color : SSColors.gray6.color
+            }
+            .disposed(by: viewModel.disposeBag)
+
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.endEditing(true)
     }
 
 }
