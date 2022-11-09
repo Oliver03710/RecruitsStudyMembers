01529bcb780a6a81bfe43b45e12a66c7db447c66
Oliver03710 <jhee2619@gmail.com>
2022-11-09 12:52:40 +0900

    Add Available Attribution On Base View

--- RecruitsStudyMembers/Utilities/BaseComponents/BaseView.swift
+++ RecruitsStudyMembers/Utilities/BaseComponents/BaseView.swift
@@ -16,6 +16,7 @@ class BaseView: UIView {
         setConstraints()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
