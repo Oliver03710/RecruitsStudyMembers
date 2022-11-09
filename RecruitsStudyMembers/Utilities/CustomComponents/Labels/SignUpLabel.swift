5c59ccff74b3aea9f23ddb70250fbd453dea786b
Oliver03710 <jhee2619@gmail.com>
2022-11-09 14:55:19 +0900

    Create Custom Label For Sign Up View

---
+++ RecruitsStudyMembers/Utilities/CustomComponents/Labels/SignUpLabel.swift
@@ -0,0 +1,38 @@
+//
+//  SignUpLabel.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/09.
+//
+
+import UIKit
+
+class SignUpLabel: UILabel {
+
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(text: String, textFont: String, size: CGFloat, lineHeight: CGFloat) {
+        self.init()
+        
+        let style = NSMutableParagraphStyle()
+        let lineHeights = size * lineHeight
+        style.minimumLineHeight = lineHeights
+        style.maximumLineHeight = lineHeights
+        
+        guard let txtFont = UIFont(name: textFont, size: size) else { return }
+        
+        let attributedTexts = NSMutableAttributedString(string: text, attributes: [NSAttributedString.Key.font: txtFont, .paragraphStyle: style, NSAttributedString.Key.foregroundColor: SSColors.black.color,  .baselineOffset: (lineHeights - size) / 4])
+        
+        attributedText = attributedTexts
+        textAlignment = .center
+        numberOfLines = 2
+    }
+}
