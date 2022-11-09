e2735aad395b75421f37cecdecfefebf9b40dcb5
Oliver03710 <jhee2619@gmail.com>
2022-11-09 15:12:25 +0900

    Create Custrom Text Field & Set Its Font Used In Sign Up View

---
+++ RecruitsStudyMembers/Utilities/CustomComponents/SignupTextField.swift
@@ -0,0 +1,27 @@
+//
+//  SignupTextField.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/09.
+//
+
+import UIKit
+
+final class SignupTextField: UITextField {
+
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(placeHolder: String) {
+        self.init()
+        placeholder = placeHolder
+        font = UIFont(name: SSFonts.title4R14.fonts, size: SSFonts.title4R14.size)
+    }
+}
