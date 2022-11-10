7def4500d8c6f4f29f5ebf48b8e9585b3b39156a
Oliver03710 <jhee2619@gmail.com>
2022-11-10 18:51:12 +0900

    Create Navi Bar Appearance Extension

---
+++ RecruitsStudyMembers/Utilities/Extensions/UINavigationBarAppearance+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UINavigationBarAppearance+Extension.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/10.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func setNaviBar(naviTitle: String? = nil, naviBarTintColor: UIColor = SSColors.black.color) {
+        
+        let barAppearance = UINavigationBarAppearance()
+        barAppearance.backgroundColor = .systemBackground
+        navigationItem.scrollEdgeAppearance = barAppearance
+        
+        navigationItem.title = naviTitle
+        self.navigationController?.navigationBar.tintColor = naviBarTintColor
+        
+    }
+    
+}
+
