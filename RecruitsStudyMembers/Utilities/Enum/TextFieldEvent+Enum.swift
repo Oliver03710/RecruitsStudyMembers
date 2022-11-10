59f597788e8eafdfbd56a5b16e0c8cbac98d451a
Oliver03710 <jhee2619@gmail.com>
2022-11-11 01:47:26 +0900

    Create Enum For TextField Status

---
+++ RecruitsStudyMembers/Utilities/Enum/TextFieldEvent+Enum.swift
@@ -0,0 +1,13 @@
+//
+//  TextFieldEvent+Enum.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/11.
+//
+
+import Foundation
+
+enum TextFieldActions {
+  case editingDidBegin
+  case editingDidEnd
+}
