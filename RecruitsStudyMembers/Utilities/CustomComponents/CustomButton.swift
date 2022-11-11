52f8a435c736b3b012ef5dace4994b3ab90980f8
Oliver03710 <jhee2619@gmail.com>
2022-11-11 21:20:35 +0900

    Set Button With Configuration

--- RecruitsStudyMembers/Utilities/CustomComponents/CustomButton.swift
+++ RecruitsStudyMembers/Utilities/CustomComponents/CustomButton.swift
@@ -26,6 +26,34 @@ class CustomButton: UIButton {
         setTitle(text, for: .normal)
     }
     
+    convenience init(text: String, borderColor: UIColor? = SSColors.gray3.color, image: String) {
+        self.init()
+        var configuration = UIButton.Configuration.plain()
+        let style = NSMutableParagraphStyle()
+        let lineHeights = SSFonts.title2R16.size * SSFonts.title2R16.lineHeight
+        style.minimumLineHeight = lineHeights
+        style.maximumLineHeight = lineHeights
+        
+        var container = AttributeContainer()
+        container.font = UIFont(name: SSFonts.title2R16.fonts, size: SSFonts.title2R16.size)
+        container.foregroundColor = SSColors.black.color
+        container.paragraphStyle = style
+        container.baselineOffset = (lineHeights - SSFonts.title2R16.size) / 4
+        
+        configuration.attributedTitle = AttributedString(text, attributes: container)
+        configuration.imagePlacement = .top
+        configuration.imagePadding = 5
+        configuration.titleAlignment = .center
+        
+        setImage(UIImage(named: image), for: .normal)
+        self.configuration = configuration
+        
+        tintColor = SSColors.white.color
+        
+        layer.borderWidth = 1
+        layer.borderColor = borderColor?.cgColor
+    }
+    
     
     // MARK: - Helper Functions
     
