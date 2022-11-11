c2a0a1a107f1daef31530c93c42a7e0f068dc465
Oliver03710 <jhee2619@gmail.com>
2022-11-11 21:13:58 +0900

    Add Setting Navigation Bar Code

--- RecruitsStudyMembers/Utilities/BaseComponents/BaseViewController.swift
+++ RecruitsStudyMembers/Utilities/BaseComponents/BaseViewController.swift
@@ -9,12 +9,31 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
+    // MARK: - Init
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
         setConstraints()
     }
     
+    
+    // MARK: - Selectors
+    
+    @objc func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+    // MARK: - Helper Functions
+    
     func configureUI () { }
     func setConstraints() { }
+    
+    func setNaigations() {
+        setNaviBar()
+        
+        let backButton = UIBarButtonItem(image: UIImage(named: GeneralIcons.arrow.rawValue), style: .plain, target: self, action: #selector(backButtonTapped))
+        self.navigationItem.leftBarButtonItem  = backButton
+    }
 }
