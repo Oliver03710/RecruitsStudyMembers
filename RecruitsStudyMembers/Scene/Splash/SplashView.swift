53f72d7ae52e4381940bf280d2990f84e967c8e5
Oliver03710 <jhee2619@gmail.com>
2022-11-13 12:21:49 +0900

    Remove Required Init

--- RecruitsStudyMembers/Scene/Splash/SplashView.swift
+++ RecruitsStudyMembers/Scene/Splash/SplashView.swift
@@ -32,10 +32,6 @@ final class SplashView: BaseView {
         setConstraints()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     
     // MARK: - Helper Functions
     
