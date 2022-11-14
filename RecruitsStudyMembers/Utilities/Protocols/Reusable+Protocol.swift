107bf1243e7eac71d06ef119035264ab66242447
Oliver03710 <jhee2619@gmail.com>
2022-11-15 04:17:15 +0900

    Create Reusable Protocol

---
+++ RecruitsStudyMembers/Utilities/Protocols/Reusable+Protocol.swift
@@ -0,0 +1,24 @@
+//
+//  Reusable+Protocol.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/15.
+//
+
+import UIKit
+
+private protocol ReusableViewProtocol {
+    static var reuseIdentifier: String { get }
+}
+
+extension UIViewController: ReusableViewProtocol {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: ReusableViewProtocol {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
