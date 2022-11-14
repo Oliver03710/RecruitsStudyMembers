1876d26a4ff9bebf92daa117a53ad1e8257322e9
Oliver03710 <jhee2619@gmail.com>
2022-11-14 22:30:22 +0900

    Set Logic To Get New ID Token And Organise Files

--- RecruitsStudyMembers/Scene/SignUp/Login/Verification/LoginVerificationViewController.swift
+++ RecruitsStudyMembers/Scene/SignUp/Login/Verification/LoginVerificationViewController.swift
@@ -147,26 +147,41 @@ final class LoginVerificationViewController: BaseViewController {
     func requestCheckUser() {
         NetworkManager.shared.request(UserData.self, router: SeSacApi.login)
             .subscribe(onSuccess: { response in
-                print("Succeed")
                 print(response)
+                let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                let sceneDelegate = windowScene?.delegate as? SceneDelegate
+                let vc = MainTabBarController()
+                sceneDelegate?.window?.rootViewController = vc
+                sceneDelegate?.window?.makeKeyAndVisible()
+                
             }, onFailure: { [weak self] error in
-                print("Error")
                 let errors = (error as NSError).code
                 print(errors)
                 guard let errCode = SeSacError(rawValue: errors) else { return }
                 switch errCode {
+                    
                 case .firebaseTokenError:
-                    self?.view.makeToast(errCode.errorDescription)
+                    guard let codeNum = NetworkManager.shared.refreshToken() else {
+                        self?.requestCheckUser()
+                        return
+                    }
+                    guard let errorCode = AuthErrorCode.Code(rawValue: codeNum) else { return }
+                    switch errorCode {
+                    case .tooManyRequests:
+                        self?.view.makeToast("과도한 인증 시도가 있었습니다. 나중에 다시 시도해 주세요.", position: .center)
+                    default:
+                        self?.view.makeToast("에러가 발생했습니다. 다시 시도해주세요.")
+                    }
+
                 case .unsignedupUser:
                     self?.view.makeToast(errCode.errorDescription, position: .top) { _ in
-                        UserDefaultsManager.statusCode = 406
                         let vc = NicknameViewController()
                         self?.navigationController?.pushViewController(vc, animated: true)
                     }
-                case .ServerError:
+                    
+                case .ServerError, .ClientError:
                     self?.view.makeToast(errCode.errorDescription)
-                case .ClientError:
-                    self?.view.makeToast(errCode.errorDescription)
+                default: break
                 }
             })
             .disposed(by: loginVerificationView.viewModel.disposeBag)
