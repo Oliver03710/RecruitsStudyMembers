a7b68b7c613bb78be8777d26585663fd9b7dd51c
Oliver03710 <jhee2619@gmail.com>
2022-11-15 04:19:48 +0900

    Set Table View For My Info Tab

--- RecruitsStudyMembers/Scene/Main/MyInfo/MyinfoViewController.swift
+++ RecruitsStudyMembers/Scene/Main/MyInfo/MyinfoViewController.swift
@@ -28,7 +28,38 @@ final class MyinfoViewController: BaseViewController {
     // MARK: - Helper Functions
     
     override func configureUI() {
-        
+        setTableViewComponents()
     }
     
+    func setTableViewComponents() {
+        myView.tableView.delegate = self
+    }
+    
+}
+
+
+// MARK: - Extension: UITableViewDelegate
+
+extension MyinfoViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            let vc = ManageInfoViewController()
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let height = myView.bounds.height
+        return indexPath.row == 0 ? height / 11 : height / 12
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == tableView.numberOfRows(inSection: indexPath.section) - 1 {
+            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: tableView.bounds.width)
+        } else {
+            cell.separatorInset = UIEdgeInsets(top: 0, left: -15, bottom: 0, right: 0)
+        }
+    }
 }
