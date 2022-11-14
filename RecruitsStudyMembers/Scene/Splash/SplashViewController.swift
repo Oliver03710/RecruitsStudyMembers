d1ffdcc66e3096564cafc544c6deb68592aca6d7
Oliver03710 <jhee2619@gmail.com>
2022-11-14 22:35:51 +0900

    Make Logic To Get Shorten Choose To View Controllers To Show

--- RecruitsStudyMembers/Scene/Splash/SplashViewController.swift
+++ RecruitsStudyMembers/Scene/Splash/SplashViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 
+import FirebaseAuth
+import RxCocoa
+import RxSwift
+
 final class SplashViewController: BaseViewController {
     
     // MARK: - Properties
     
     private let splashView = SplashView()
+    private let disposeBag = DisposeBag()
     
     
     // MARK: - Init
@@ -32,26 +37,67 @@ final class SplashViewController: BaseViewController {
     }
     
     func transitionAfterAnimation() {
-    
+        
         UIView.animate(withDuration: 2) { [weak self] () -> Void in
             self?.splashView.layoutIfNeeded()
         } completion: { [weak self] _ in
             sleep(1)
             
-            if UserDefaultsManager.passOnboarding {
+            if !UserDefaultsManager.token.isEmpty {
+                self?.requestCheckUser()
+                
+            } else if !UserDefaultsManager.passOnboarding {
+                let vc = OnboardingViewController()
+                vc.modalPresentationStyle = .overFullScreen
+                self?.present(vc, animated: true)
+                
+            } else if UserDefaultsManager.passOnboarding {
                 let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
                 let sceneDelegate = windowScene?.delegate as? SceneDelegate
                 let vc = LoginViewController()
                 let nav = UINavigationController(rootViewController: vc)
                 sceneDelegate?.window?.rootViewController = nav
                 sceneDelegate?.window?.makeKeyAndVisible()
-            } else if !UserDefaultsManager.passOnboarding {
-                let vc = OnboardingViewController()
-                vc.modalPresentationStyle = .overFullScreen
-                self?.present(vc, animated: true)
             }
         }
         
+    }
+    
+    func requestCheckUser() {
+        NetworkManager.shared.request(UserData.self, router: SeSacApi.login)
+            .subscribe(onSuccess: { response in
+                print(response)
+                let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                let sceneDelegate = windowScene?.delegate as? SceneDelegate
+                let vc = MainTabBarController()
+                sceneDelegate?.window?.rootViewController = vc
+                sceneDelegate?.window?.makeKeyAndVisible()
+                
+            }, onFailure: { [weak self] error in
+                let errors = (error as NSError).code
+                print(errors)
+                guard let errCode = SeSacError(rawValue: errors) else { return }
+                switch errCode {
+                    
+                case .firebaseTokenError:
+                    guard let codeNum = NetworkManager.shared.refreshToken() else {
+                        self?.requestCheckUser()
+                        return
+                    }
+                    guard let errorCode = AuthErrorCode.Code(rawValue: codeNum) else { return }
+                    switch errorCode {
+                    case .tooManyRequests:
+                        self?.view.makeToast("과도한 인증 시도가 있었습니다. 나중에 다시 시도해 주세요.", position: .center)
+                    default:
+                        self?.view.makeToast("에러가 발생했습니다. 다시 시도해주세요.")
+                    }
+                    
+                case .unsignedupUser, .ServerError, .ClientError:
+                    self?.view.makeToast(errCode.errorDescription)
+                default: break
+                }
+            })
+            .disposed(by: disposeBag)
     }
 
     
