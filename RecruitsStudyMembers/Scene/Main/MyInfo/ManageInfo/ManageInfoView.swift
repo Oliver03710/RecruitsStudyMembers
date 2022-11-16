255a8dea893af1a7e801d9f4db35cc6dea2a89f6
Oliver03710 <jhee2619@gmail.com>
2022-11-16 20:09:21 +0900

    Create Info Tab With Compositional Layout & Diffable Data Source

--- RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/ManageInfoView.swift
+++ RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/ManageInfoView.swift
@@ -7,37 +7,191 @@
 
 import UIKit
 
-final class ManageInfoView: BaseView {
+import SnapKit
 
+final class ManageInfoView: BaseView {
+    
+    // MARK: - Enum
+    
+    enum Section: Int, CaseIterable, Hashable {
+        case image, foldable, rest
+    }
+    
+    
     // MARK: - Properties
     
-    var collectionView: UICollectionView!
-    
-    var dataSource: UICollectionViewDiffableDataSource<Section, MyinfoView.NewsFeedItem>!
-    
-    lazy var items: [NewsFeedItem] = {
-       return itemsInternal()
+    lazy var collectionView: UICollectionView = {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        return cv
     }()
+    var isHiddens = false
+    var count = 0
     
-    var data:[PresentMenuSectionModel] = []
-
-    // 케이스 1
-    self.data.append(.SectionMainTitle(items: [PresentMenuTitleItem(image: hasImage, mainTitle: self.title, description: hasData?.description)]))
-    // 케이스 2
-    self.data.append(.SectionMenu(header: section.title, selectType: sectionType, items: menuBundle))
-    // 케이스 3
-    self.data.append(.SectionSelectCount(items: [PresentSelectCountItem(count: 1)]))
+    var dataSource: UICollectionViewDiffableDataSource<Section, DummyData>! = nil
+    var currentSnapshot = NSDiffableDataSourceSnapshot<Section, DummyData>()
+    
+    let viewModel = ManageInfoViewModel()
     
     
     // MARK: - Init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureDataSource()
+        collectionView.bounces = false
     }
     
+    override func setConstraints() {
+        addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints {
+            $0.edges.equalTo(safeAreaLayoutGuide).inset(16)
+        }
+    }
     
-    
-    
-    
+}
 
+
+// MARK: - Extension: Compositional Layout & Diffable DataSource
+
+extension ManageInfoView {
+    
+    private func createLayout() -> UICollectionViewLayout {
+        
+        let layout = UICollectionViewCompositionalLayout { (section, env) -> NSCollectionLayoutSection? in
+
+            guard let customSection = Section(rawValue: section) else { return nil }
+
+            switch customSection {
+            case .image:
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                      heightDimension: .fractionalHeight(1))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                       heightDimension: .fractionalWidth(0.56))
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize,
+                                                               subitems: [item])
+
+                let section = NSCollectionLayoutSection(group: group)
+
+                return section
+
+            case .foldable:
+                let estimatedHeight = CGFloat(50)
+                let layoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                        heightDimension: .estimated(estimatedHeight))
+                let item = NSCollectionLayoutItem(layoutSize: layoutSize)
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: layoutSize,
+                                                               subitem: item,
+                                                               count: 1)
+                let section = NSCollectionLayoutSection(group: group)
+                
+                let sectionBackgroundDecoration = NSCollectionLayoutDecorationItem.background(elementKind: "reusableView")
+                section.decorationItems = [sectionBackgroundDecoration]
+                return section
+
+            case .rest:
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .fractionalHeight(1.0))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                       heightDimension: .fractionalWidth(0.2))
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize,
+                                                               subitems: [item])
+
+                let section = NSCollectionLayoutSection(group: group)
+
+                return section
+            }
+        }
+        
+        layout.register(ReusableView.self, forDecorationViewOfKind: "reusableView")
+        return layout
+    }
+    
+    private func configureDataSource() {
+        
+        let imageCellRegistration = UICollectionView.CellRegistration<ImageCollectionViewCell, DummyData> { (cell, indexPath, identifier) in
+            cell.foreImageView.image = UIImage(named: identifier.foregroundImage!)
+            
+            
+            
+            var backConfig = UIBackgroundConfiguration.listPlainCell()
+            backConfig.image = UIImage(named: identifier.backgroundImage!)
+            backConfig.cornerRadius = 8
+            cell.backgroundConfiguration = backConfig
+            
+        }
+        
+        let foldableCellRegistration = UICollectionView.CellRegistration<FoldableCollectionViewCell, DummyData> { (cell, indexPath, identifier) in
+            
+
+            
+            cell.nameLabel.text = identifier.name
+            cell.titleLabel.text = identifier.title
+            cell.configureCell(indexPath: indexPath, bool: self.isHiddens, count: self.count)
+//            cell.configureCell(indexPath: indexPath, bool: self.isHiddens)
+//            cell.configureCell(isHidden: self.isHiddens)
+//            var backConfig = UIBackgroundConfiguration.listPlainCell()
+//            backConfig.cornerRadius = 8
+            
+//            view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+//            backConfig.strokeColor = SSColors.gray2.color
+//            backConfig.strokeWidth = 1
+//            cell.backgroundConfiguration = backConfig
+        }
+
+        let restCellRegistration = UICollectionView.CellRegistration<UICollectionViewCell, DummyData> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+//            cell.label.text = "\(identifier)"
+//            cell.contentView.backgroundColor = SSColors.yellowGreen.color
+//            cell.layer.borderColor = UIColor.black.cgColor
+//            cell.layer.borderWidth = 1
+//            cell.label.textAlignment = .center
+//            cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, DummyData>(collectionView: collectionView)
+        { (collectionView, indexPath, identifier) -> UICollectionViewCell? in
+            
+            guard let customSection = Section(rawValue: indexPath.section) else { return nil }
+            
+            switch customSection {
+            case .image:
+                print(indexPath)
+                return collectionView.dequeueConfiguredReusableCell(using: imageCellRegistration, for: indexPath, item: identifier)
+            case .foldable:
+                print(indexPath)
+                return collectionView.dequeueConfiguredReusableCell(using: foldableCellRegistration, for: indexPath, item: identifier)
+            case .rest:
+                print(indexPath)
+                return collectionView.dequeueConfiguredReusableCell(using: imageCellRegistration, for: indexPath, item: identifier)
+            }
+        }
+        updateUI()
+
+//        currentSnapshot = NSDiffableDataSourceSnapshot<Section, DummyData>()
+//        Section.allCases.forEach { section in
+//            currentSnapshot.appendSections([section])
+//            currentSnapshot.appendItems(DummyData.callDummy(), toSection: section)
+//        }
+//        dataSource.apply(currentSnapshot, animatingDifferences: true)
+
+    }
+    
+    func updateUI() {
+        currentSnapshot = NSDiffableDataSourceSnapshot<Section, DummyData>()
+        currentSnapshot.appendSections(Section.allCases)
+        currentSnapshot.appendItems(DummyData.callDummy(), toSection: .image)
+        currentSnapshot.appendItems(DummyData.callDummy(), toSection: .rest)
+        if !isHiddens {
+            currentSnapshot.appendItems(DummyData.callDummy(), toSection: .foldable)
+        } else {
+            currentSnapshot.appendItems(DummyData.diffDummy(), toSection: .foldable)
+        }
+        dataSource.apply(currentSnapshot, animatingDifferences: true)
+    }
+    
 }
--- RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/ManageInfoViewController.swift
+++ RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/ManageInfoViewController.swift
@@ -7,23 +7,65 @@
 
 import UIKit
 
-class ManageInfoViewController: BaseViewController {
+import RxCocoa
+import RxSwift
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+final class ManageInfoViewController: BaseViewController {
 
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    
+    private let myView = ManageInfoView()
+    
+    
+    // MARK: - Init
+    
+    override func loadView() {
+        view = myView
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    
+    // MARK: - Selectors
+    
+    @objc func saveAction() {
+        
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    override func configureUI() {
+        setNavigationStatus()
+        myView.collectionView.delegate = self
+    }
+    
+    func setNavigationStatus() {
+        setNaigations(naviTitle: "정보 관리")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(saveAction))
+    }
+    
+    func bindData() {
+        
+    }
 
+}
+
+
+extension ManageInfoViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: false)
+        myView.isHiddens = myView.isHiddens ? false : true
+        myView.count += 1
+        myView.updateUI()
+    }
 }
