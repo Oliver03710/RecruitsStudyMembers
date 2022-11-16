afa44342ed59c1eda5dd73944e72c8885ee493b2
Oliver03710 <jhee2619@gmail.com>
2022-11-16 20:10:58 +0900

    Create ReusableView To Make Background For Foldable Section In Info Tab

--- RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/ReusableView.swift
+++ RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/ReusableView.swift
@@ -5,4 +5,24 @@
 //  Created by Junhee Yoon on 2022/11/16.
 //
 
-import Foundation
+import UIKit
+
+class ReusableView: UICollectionReusableView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("not implemented")
+    }
+}
+
+extension ReusableView {
+    func configure() {
+//        backgroundColor = UIColor.lightGray.withAlphaComponent(0.5)
+        layer.borderColor = UIColor.red.cgColor
+        layer.borderWidth = 1
+        layer.cornerRadius = 8
+    }
+}
