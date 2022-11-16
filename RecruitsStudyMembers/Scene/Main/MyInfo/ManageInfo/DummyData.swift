c6dec0c530cf9f64a2134f726f48b56fe16fcff0
Oliver03710 <jhee2619@gmail.com>
2022-11-16 20:08:28 +0900

    Create Dummy Data For Test

--- RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/DummyData.swift
+++ RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/DummyData.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct DummyData: Hashable {
+    let backgroundImage: String?
+    let foregroundImage: String?
+    let name: String?
+    let title: String?
+    private let identifier = UUID()
+    
+    static func callDummy() -> [DummyData] {
+        return [DummyData(backgroundImage: BackgroundImages.sesacBackground1.rawValue,foregroundImage: FaceImages.sesacFace1.rawValue , name: "김새싹", title: nil)]
+    }
+    
+    static func diffDummy() -> [DummyData] {
+        return [DummyData(backgroundImage: BackgroundImages.sesacBackground1.rawValue,foregroundImage: FaceImages.sesacFace1.rawValue , name: "김새싹", title: nil),
+                DummyData(backgroundImage: BackgroundImages.sesacBackground1.rawValue,foregroundImage: FaceImages.sesacFace1.rawValue , name: "김새싹", title: "타이틀"),
+                DummyData(backgroundImage: BackgroundImages.sesacBackground1.rawValue,foregroundImage: FaceImages.sesacFace1.rawValue , name: "김새싹", title: "타이틀")]
+    }
+
+}
