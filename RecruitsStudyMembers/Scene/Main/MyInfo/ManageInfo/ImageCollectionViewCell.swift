64b22c6e85e988d6bf6c74353c1a39c72f90adf0
Oliver03710 <jhee2619@gmail.com>
2022-11-16 20:10:12 +0900

    Set Image Section In Info Tab

--- RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/ImageCollectionViewCell.swift
+++ RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/ImageCollectionViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  ManageInfoCollectionViewCell.swift
+//  ImageCollectionViewCell.swift
 //  RecruitsStudyMembers
 //
 //  Created by Junhee Yoon on 2022/11/14.
@@ -20,12 +20,6 @@ final class ImageCollectionViewCell: UICollectionViewCell {
         iv.contentMode = .scaleAspectFit
         return iv
     }()
-    
-    let nameLabel: UILabel = {
-        let label = UILabel()
-        label.isHidden = true
-        return label
-    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -37,18 +31,12 @@ final class ImageCollectionViewCell: UICollectionViewCell {
     }
     
     private func setConstraints() {
-        [foreImageView, nameLabel].forEach { contentView.addSubview($0) }
+        [foreImageView].forEach { contentView.addSubview($0) }
         
         foreImageView.snp.makeConstraints {
-            $0.centerX.equalTo(self.snp.centerX)
-            $0.centerY.equalTo(self.snp.centerY).multipliedBy(1.2)
-            $0.width.equalTo(self.snp.width).dividedBy(2)
-            $0.height.equalTo(foreImageView.snp.width)
+            $0.edges.equalTo(safeAreaLayoutGuide)
         }
         
-        nameLabel.snp.makeConstraints {
-            $0.edges.equalTo(self.snp.edges)
-        }
     }
     
 }
