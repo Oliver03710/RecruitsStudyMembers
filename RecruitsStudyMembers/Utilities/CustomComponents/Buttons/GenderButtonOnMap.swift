515ce574fa3ba5ab1a16b8f3bd9fca9318231223
Oliver03710 <jhee2619@gmail.com>
2022-11-20 01:57:27 +0900

    Create Button To Customise

--- RecruitsStudyMembers/Utilities/CustomComponents/Buttons/GenderButtonOnMap.swift
+++ RecruitsStudyMembers/Utilities/CustomComponents/Buttons/GenderButtonOnMap.swift
@@ -5,4 +5,25 @@
 //  Created by Junhee Yoon on 2022/11/20.
 //
 
-import Foundation
+import UIKit
+
+final class GenderButtonOnMap: UIButton {
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(text: String, textColor: UIColor? = SSColors.black.color) {
+        self.init()
+        setTitle(text, for: .normal)
+        backgroundColor = SSColors.white.color
+        setTitleColor(textColor, for: .normal)
+    }
+    
+}
