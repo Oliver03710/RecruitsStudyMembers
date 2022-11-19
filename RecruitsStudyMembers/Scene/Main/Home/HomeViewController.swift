80b07cf92080d15dc396dba5baf500acbfae8823
Oliver03710 <jhee2619@gmail.com>
2022-11-19 22:50:28 +0900

    Show Current Location With Custom Image At The Center Of The Screen & Move Current Position When Region Changed

--- RecruitsStudyMembers/Scene/Main/Home/HomeViewController.swift
+++ RecruitsStudyMembers/Scene/Main/Home/HomeViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
+
+import Toast
 
 final class HomeViewController: BaseViewController {
 
@@ -30,10 +34,122 @@ final class HomeViewController: BaseViewController {
     
     override func configureUI() {
         setNaigations()
+        setDelegates()
     }
     
     override func setNaigations(naviTitle: String? = nil) {
         navigationController?.isNavigationBarHidden = true
+    }
+    
+    func setDelegates() {
+        myView.mapView.delegate = self
+        myView.locationManager.delegate = self
+    }
+    
+    func setAnnotations() {
+        let annotation = SeSacAnnotation(0)
+        annotation.coordinate = myView.mapView.region.center
+        myView.mapView.addAnnotation(annotation)
+    }
+    
+    func removeAllAnnotations() {
+        myView.mapView.annotations.forEach { (annotation) in
+            if let annotation = annotation as? MKPointAnnotation {
+                myView.mapView.removeAnnotation(annotation)
+            }
+        }
+    }
+}
+
+
+// MARK: - Authorizations
+
+extension HomeViewController {
+    
+    func showRequestLocationServiceAlert() {
+        
+      let requestLocationServiceAlert = UIAlertController(title: "위치정보 이용", message: "위치 서비스를 사용할 수 없습니다. 기기의 '설정>개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
+        
+      let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+          if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+              UIApplication.shared.open(appSetting)
+          }
+      }
+        
+      let cancel = UIAlertAction(title: "취소", style: .default)
+      requestLocationServiceAlert.addAction(cancel)
+      requestLocationServiceAlert.addAction(goSetting)
+      
+      present(requestLocationServiceAlert, animated: true, completion: nil)
+        
+    }
+}
+
+
+// MARK: - Extension: MKMapViewDelegate
+
+extension HomeViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        removeAllAnnotations()
+        setAnnotations()
+    }
+    
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? SeSacAnnotation else { return nil }
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "\(annotation.identifier)")
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "\(annotation.identifier)")
+        } else {
+            annotationView?.annotation = annotation
+        }
+        annotationView?.image = UIImage(named: GeneralIcons.mapMarker.rawValue)
+
+        return annotationView
+    }
+
+}
+
+
+// MARK: - Extension: CLLocationManagerDelegate
+
+extension HomeViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let coordinate = locations.last?.coordinate {
+            let region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            myView.mapView.setRegion(region, animated: true)
+        }
+        
+        myView.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        view.makeToast("위치 이동에 실패")
+    }
+        
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if #available(iOS 14.0, *) {
+            switch manager.authorizationStatus {
+            case .notDetermined:
+                myView.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                myView.locationManager.requestWhenInUseAuthorization()
+            case .restricted, .denied:
+                print("Denied, 아이폰 설정으로 유도")
+                showRequestLocationServiceAlert()
+            case .authorizedWhenInUse:
+                print("When In Use")
+                myView.locationManager.startUpdatingLocation()
+            default:
+                break
+            }
+        }
     }
     
 }
