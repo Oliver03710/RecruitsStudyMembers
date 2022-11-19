acdc366e031df256b5697c6b61253dd12330a016
Oliver03710 <jhee2619@gmail.com>
2022-11-20 02:42:30 +0900

    Create Button(Choose Gender & Show Current Location)

--- RecruitsStudyMembers/Scene/Main/Home/HomeView.swift
+++ RecruitsStudyMembers/Scene/Main/Home/HomeView.swift
@@ -13,7 +13,7 @@ import MapKit
 import SnapKit
 
 final class HomeView: BaseView {
-
+    
     // MARK: - Properties
     
     let mapView: MKMapView = {
@@ -27,6 +27,39 @@ final class HomeView: BaseView {
         return lm
     }()
     
+    lazy var verticalStackView: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [allGenderButton, maleButton, femaleButton])
+        sv.backgroundColor = SSColors.white.color
+        sv.axis = .vertical
+        sv.distribution = .fillEqually
+        sv.spacing = 0
+        sv.clipsToBounds = true
+        sv.layer.cornerRadius = 8
+        
+        return sv
+    }()
+    
+    private let allGenderButton: GenderButtonOnMap = {
+        let btn = GenderButtonOnMap(text: "전체", isSelected: true)
+        return btn
+    }()
+    
+    private let maleButton: GenderButtonOnMap = {
+        let btn = GenderButtonOnMap(text: "남자")
+        return btn
+    }()
+    
+    private let femaleButton: GenderButtonOnMap = {
+        let btn = GenderButtonOnMap(text: "여자")
+        return btn
+    }()
+    
+    private let currentButton: CustomButton = {
+        let btn = CustomButton(image: GeneralIcons.place.rawValue)
+        return btn
+    }()
+    
+    
     // MARK: - Init
     
     override init(frame: CGRect) {
@@ -38,11 +71,24 @@ final class HomeView: BaseView {
     
     override func setConstraints() {
         addSubview(mapView)
+        [verticalStackView, currentButton].forEach { mapView.addSubview($0) }
         
-        mapView.snp.makeConstraints { make in
-            make.top.equalTo(self.snp.top)
-            make.directionalHorizontalEdges.equalTo(self.snp.directionalHorizontalEdges)
-            make.bottom.equalTo(safeAreaLayoutGuide)
+        mapView.snp.makeConstraints {
+            $0.top.equalTo(self.snp.top)
+            $0.directionalHorizontalEdges.equalTo(self.snp.directionalHorizontalEdges)
+            $0.bottom.equalTo(safeAreaLayoutGuide)
+        }
+        
+        verticalStackView.snp.makeConstraints {
+            $0.top.leading.equalTo(safeAreaLayoutGuide).inset(16)
+            $0.width.equalTo(48)
+            $0.height.equalTo(verticalStackView.snp.width).multipliedBy(3)
+        }
+        
+        currentButton.snp.makeConstraints {
+            $0.leading.equalTo(safeAreaLayoutGuide).inset(16)
+            $0.top.equalTo(verticalStackView.snp.bottom).offset(16)
+            $0.width.height.equalTo(48)
         }
     }
 }
