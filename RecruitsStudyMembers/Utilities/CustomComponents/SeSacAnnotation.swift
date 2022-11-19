f09c9100dad19fdcc36ef85b6c74412808924009
Oliver03710 <jhee2619@gmail.com>
2022-11-19 21:45:21 +0900

    Create Custom Annotation

---
+++ RecruitsStudyMembers/Utilities/CustomComponents/SeSacAnnotation.swift
@@ -0,0 +1,16 @@
+//
+//  CustomAnnotation.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/19.
+//
+
+import MapKit
+
+final class SeSacAnnotation: MKPointAnnotation {
+    var identifier: Int
+    
+    init(_ identifier: Int) {
+        self.identifier = identifier
+    }
+}
