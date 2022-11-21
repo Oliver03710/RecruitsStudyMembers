73f90cbca8481b76ffdcf8bb2ada6ba95eaa4f27
Oliver03710 <jhee2619@gmail.com>
2022-11-21 17:54:38 +0900

    Create Search View

---
+++ RecruitsStudyMembers/Scene/Main/Home/Search/SearchView.swift
@@ -0,0 +1,30 @@
+//
+//  SearchView.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/21.
+//
+
+import UIKit
+
+final class SearchView: BaseView {
+    
+    // MARK: - Properties
+    
+    
+    
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    override func setConstraints() {
+        
+    }
+
+}
