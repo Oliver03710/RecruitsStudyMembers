8eb0c3129b4292ed1d6231401aaf67e7154fd1ea
Oliver03710 <jhee2619@gmail.com>
2022-11-22 01:17:23 +0900

    Set UI Components

--- RecruitsStudyMembers/Scene/Main/Home/Search/SearchView.swift
+++ RecruitsStudyMembers/Scene/Main/Home/Search/SearchView.swift
@@ -18,6 +18,24 @@ final class SearchView: BaseView {
         return btn
     }()
     
+    let accButton: UIButton = {
+        let btn = UIButton(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 48))
+        btn.backgroundColor = SSColors.green.color
+        btn.setTitle("새싹 찾기", for: .normal)
+        return btn
+    }()
+    
+    let searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.sizeToFit()
+        return sb
+    }()
+    
+    let tap: UITapGestureRecognizer = {
+        let tap = UITapGestureRecognizer()
+        return tap
+    }()
+    
     
     // MARK: - Init
     
@@ -30,9 +48,10 @@ final class SearchView: BaseView {
     
     override func setConstraints() {
         addSubview(seekButton)
+        addGestureRecognizer(tap)
         
         seekButton.snp.makeConstraints {
-            $0.directionalHorizontalEdges.bottom.equalTo(safeAreaInsets)
+            $0.directionalHorizontalEdges.bottom.equalTo(safeAreaLayoutGuide).inset(16)
             $0.height.equalTo(48)
         }
     }
--- RecruitsStudyMembers/Scene/Main/Home/Search/SearchViewController.swift
+++ RecruitsStudyMembers/Scene/Main/Home/Search/SearchViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+import SnapKit
+
 final class SearchViewController: BaseViewController {
     
     // MARK: - Properties
     
     private let myView = SearchView()
+    private let disposeBag = DisposeBag()
     
     
     // MARK: - Init
@@ -29,9 +34,61 @@ final class SearchViewController: BaseViewController {
         super.viewWillAppear(animated)
         self.tabBarController?.tabBar.isHidden = true
     }
-
+    
+    
+    // MARK: - Selectors
+    
+    @objc func endEdit() {
+        myView.searchBar.resignFirstResponder()
+    }
+    
     
     // MARK: - Helper Functions
     
+    override func configureUI() {
+        settingNavibars()
+        bindData()
+        tapGesture()
+    }
+    
+    private func settingNavibars() {
+        let navigationBar = navigationController?.navigationBar
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.shadowColor = .clear
+        navigationBarAppearance.backgroundColor = SSColors.white.color
+        navigationBar?.scrollEdgeAppearance = navigationBarAppearance
+        
+        navigationItem.title = nil
+        navigationController?.navigationBar.tintColor = SSColors.black.color
+        navigationController?.isNavigationBarHidden = false
+        
+        let backButton = UIBarButtonItem(image: UIImage(named: GeneralIcons.arrow.rawValue), style: .plain, target: self, action: #selector(backButtonTapped))
+        navigationItem.leftBarButtonItem  = backButton
+        
+        navigationItem.titleView = myView.searchBar
+    }
+    
+    private func bindData() {
+        Observable.merge(myView.searchBar.rx.textDidBeginEditing.map { _ in TextFieldActions.editingDidBegin },
+                         myView.searchBar.rx.textDidEndEditing.map { _ in TextFieldActions.editingDidEnd })
+        .asDriver(onErrorJustReturn: .editingDidBegin)
+        .drive { [weak self] actions in
+            guard let self = self else { return }
+            switch actions {
+            case .editingDidBegin:
+                self.myView.seekButton.isHidden = true
+                self.myView.searchBar.searchTextField.inputAccessoryView = self.myView.accButton
+                
+            case .editingDidEnd:
+                self.myView.seekButton.isHidden = false
+            }
+        }
+        .disposed(by: disposeBag)
+
+    }
+    
+    func tapGesture() {
+        myView.tap.addTarget(self, action: #selector(endEdit))
+    }
     
 }
