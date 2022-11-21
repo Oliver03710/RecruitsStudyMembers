be5636a3c7859a565113b59f8ec87a625a50f374
Oliver03710 <jhee2619@gmail.com>
2022-11-21 17:37:41 +0900

    Create Search View Controller

---
+++ RecruitsStudyMembers/Scene/Main/Home/Search/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SearchViewController.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/21.
+//
+
+import UIKit
+
+final class SearchViewController: BaseViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Helper Functions
+    
+    
+}
