071a6504143e86c5f94cc9de120105bff4f5182b
Oliver03710 <jhee2619@gmail.com>
2022-11-23 04:41:25 +0900

    Put Request

--- RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/ManageInfoViewController.swift
+++ RecruitsStudyMembers/Scene/Main/MyInfo/ManageInfo/ManageInfoViewController.swift
@@ -38,7 +38,40 @@ final class ManageInfoViewController: BaseViewController {
     // MARK: - Selectors
     
     @objc func saveAction() {
-        
+        NetworkManager.shared.request(router: SeSacApi.myPage)
+            .subscribe(onSuccess: { [weak self] response in
+                print(response)
+                self?.view.makeToast("업데이트 성공!") { [weak self] _ in
+                    self?.navigationController?.popViewController(animated: true)
+                }
+                
+            }, onFailure: { [weak self] error in
+                let errors = (error as NSError).code
+                print(errors)
+                guard let errCode = SeSacError(rawValue: errors) else { return }
+                switch errCode {
+                    
+                case .firebaseTokenError:
+                    guard let codeNum = NetworkManager.shared.refreshToken() else {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+                            self?.login()
+                        }
+                        return
+                    }
+                    guard let errorCode = AuthErrorCode.Code(rawValue: codeNum) else { return }
+                    switch errorCode {
+                    case .tooManyRequests:
+                        self?.view.makeToast("과도한 인증 시도가 있었습니다. 나중에 다시 시도해 주세요.", position: .center)
+                    default:
+                        self?.view.makeToast("에러가 발생했습니다. 다시 시도해주세요.")
+                    }
+                    
+                case .unsignedupUser, .ServerError, .ClientError:
+                    self?.view.makeToast(errCode.errorDescription)
+                default: break
+                }
+            })
+            .disposed(by: myView.viewModel.disposeBag)
     }
     
     
@@ -63,7 +96,10 @@ final class ManageInfoViewController: BaseViewController {
     func login() {
         NetworkManager.shared.request(UserData.self, router: SeSacApi.login)
             .subscribe(onSuccess: { response in
-                NetworkManager.shared.userData = response
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+                    NetworkManager.shared.userData = response
+                }
+                print(response)
                 
             }, onFailure: { [weak self] error in
                 let errors = (error as NSError).code
