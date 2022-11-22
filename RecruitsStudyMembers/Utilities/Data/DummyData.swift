a470b9a17e37ad69e1eb8dd5ab8df775e119769a
Oliver03710 <jhee2619@gmail.com>
2022-11-22 17:48:21 +0900

    Reset Dummy Data

--- RecruitsStudyMembers/Utilities/Data/DummyData.swift
+++ RecruitsStudyMembers/Utilities/Data/DummyData.swift
@@ -20,10 +20,8 @@ struct DummyData: Hashable {
     
     static func diffDummy() -> [DummyData] {
         return [DummyData(backgroundImage: BackgroundImages.sesacBackground1.rawValue,foregroundImage: FaceImages.sesacFace1.rawValue , name: "김새싹", title: nil),
-                DummyData(backgroundImage: BackgroundImages.sesacBackground1.rawValue,foregroundImage: FaceImages.sesacFace1.rawValue , name: "김새싹", title: "타이틀"),
                 DummyData(backgroundImage: BackgroundImages.sesacBackground1.rawValue,foregroundImage: FaceImages.sesacFace1.rawValue , name: "김새싹", title: "타이틀")]
     }
-
 }
 
 
@@ -33,17 +31,16 @@ struct DummyItem: Hashable {
     private let identifier = UUID()
     
     static func callDummy() -> [DummyItem] {
-        return [DummyItem(text: "trash"),
-                DummyItem(text: "folder"),
-                DummyItem(text: "paperplane"),
-                DummyItem(text: "book"),
-                DummyItem(text: "tag"),
-                DummyItem(text: "camera"),
-                DummyItem(text: "pin"),
-                DummyItem(text: "lock.shield"),
-                DummyItem(text: "cube.box"),
-                DummyItem(text: "gift"),
-                DummyItem(text: "eyeglasses"),
-                DummyItem(text: "lightbulb")]
+        return [DummyItem(text: "Swift"),
+                DummyItem(text: "SwiftUI"),
+                DummyItem(text: "CoreData"),
+                DummyItem(text: "Python"),
+                DummyItem(text: "Java")]
+    }
+    
+    static func baseDummy() -> [DummyItem] {
+        return [DummyItem(text: "아무거나"),
+                DummyItem(text: "SeSAC"),
+                DummyItem(text: "코딩")]
     }
 }
