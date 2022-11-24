d4917254642464f7402018d7b59040fa877ea4b9
Oliver03710 <jhee2619@gmail.com>
2022-11-25 02:58:47 +0900

    Create Location Manager

---
+++ RecruitsStudyMembers/Utilities/Managers/LocationManager.swift
@@ -0,0 +1,26 @@
+//
+//  LocationManager.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/25.
+//
+
+import Foundation
+
+final class LocationManager {
+    
+    // MARK: - Properties
+    
+    static let shared = LocationManager()
+    
+    
+    // MARK: - Init
+    
+    private init() { }
+    
+    
+    // MARK: - Helper Functions
+    
+    
+    
+}
