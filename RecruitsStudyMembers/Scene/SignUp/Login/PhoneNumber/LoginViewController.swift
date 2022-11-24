0592d99b68ecbe593c092214566db333e670c0a6
Oliver03710 <jhee2619@gmail.com>
2022-11-24 18:02:56 +0900

    Add Access Control Keywords

--- RecruitsStudyMembers/Scene/SignUp/Login/PhoneNumber/LoginViewController.swift
+++ RecruitsStudyMembers/Scene/SignUp/Login/PhoneNumber/LoginViewController.swift
@@ -16,13 +16,13 @@ final class LoginViewController: BaseViewController {
 
     // MARK: - Properties
     
-    private let loginView = LoginView()
+    private let myView = LoginView()
     
     
     // MARK: - Init
     
     override func loadView() {
-        view = loginView
+        view = myView
     }
     
     override func viewDidLoad() {
@@ -37,49 +37,49 @@ final class LoginViewController: BaseViewController {
         bindData()
     }
     
-    func bindData() {
-        let input = LoginViewModel.Input(textFieldText: loginView.phoneNumTextField.rx.text,
-                                         textFieldIsEditing: loginView.phoneNumTextField.rx.controlEvent(.editingDidBegin),
-                                         textFieldFiniedEditing: loginView.phoneNumTextField.rx.controlEvent(.editingDidEnd),
-                                         tap: loginView.getCertiNumButton.rx.tap)
-        let output = loginView.viewModel.transform(input: input)
+    private func bindData() {
+        let input = LoginViewModel.Input(textFieldText: myView.phoneNumTextField.rx.text,
+                                         textFieldIsEditing: myView.phoneNumTextField.rx.controlEvent(.editingDidBegin),
+                                         textFieldFiniedEditing: myView.phoneNumTextField.rx.controlEvent(.editingDidEnd),
+                                         tap: myView.getCertiNumButton.rx.tap)
+        let output = myView.viewModel.transform(input: input)
         
         output.phoneNum
             .withUnretained(self)
             .bind { (vc, bool) in
-                vc.loginView.getCertiNumButton.backgroundColor = bool ? SSColors.green.color : SSColors.gray6.color
+                vc.myView.getCertiNumButton.backgroundColor = bool ? SSColors.green.color : SSColors.gray6.color
             }
-            .disposed(by: loginView.viewModel.disposeBag)
+            .disposed(by: myView.viewModel.disposeBag)
         
         output.textChanged
-            .bind(to: loginView.phoneNumTextField.rx.text)
-            .disposed(by: loginView.viewModel.disposeBag)
+            .bind(to: myView.phoneNumTextField.rx.text)
+            .disposed(by: myView.viewModel.disposeBag)
         
         output.textFieldActions
             .withUnretained(self)
             .bind { (vc, actions) in
                 switch actions {
                 case .editingDidBegin:
-                    vc.loginView.lineView.backgroundColor = SSColors.black.color
+                    vc.myView.lineView.backgroundColor = SSColors.black.color
                 case .editingDidEnd:
-                    vc.loginView.lineView.backgroundColor = SSColors.gray6.color
+                    vc.myView.lineView.backgroundColor = SSColors.gray6.color
                 }
             }
-            .disposed(by: loginView.viewModel.disposeBag)
+            .disposed(by: myView.viewModel.disposeBag)
 
         output.tapDriver
             .throttle(.seconds(3))
             .drive { [weak self] _ in
                 output.buttonValid.value ? self?.toNextPage() : self?.view.makeToast("잘못된 전화번호 형식입니다.", position: .top)
             }
-            .disposed(by: loginView.viewModel.disposeBag)
+            .disposed(by: myView.viewModel.disposeBag)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
     
-    func toNextPage() {
+    private func toNextPage() {
         view.makeToast("전화번호 인증 시작", position: .center)
         
         PhoneAuthProvider.provider()
