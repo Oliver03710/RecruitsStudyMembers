4d61211c0dd94aaf36a745f240a5a5aab30b49b1
Oliver03710 <jhee2619@gmail.com>
2022-11-25 04:25:23 +0900

    Transform Taps

--- RecruitsStudyMembers/Scene/Main/Home/HomeViewModel.swift
+++ RecruitsStudyMembers/Scene/Main/Home/HomeViewModel.swift
@@ -20,16 +20,23 @@ final class HomeViewModel: CommonViewModel {
     // MARK: - In & Out Data
     
     struct Input {
+        let currentButtonTapped: ControlEvent<Void>
+        let seekButtonTapped: ControlEvent<Void>
     }
     
     struct Output {
+        let currentButtonDriver : SharedSequence<DriverSharingStrategy, Void>
+        let seekButtonDriver: SharedSequence<DriverSharingStrategy, Void>
     }
     
     
     // MARK: - Helper Functions
     
     func transform(input: Input) -> Output {
+        
+        let currentButtonDriver = input.currentButtonTapped.asDriver()
+        let seekButtonDriver = input.seekButtonTapped.asDriver()
        
-        return Output()
+        return Output(currentButtonDriver: currentButtonDriver, seekButtonDriver: seekButtonDriver)
     }
 }
