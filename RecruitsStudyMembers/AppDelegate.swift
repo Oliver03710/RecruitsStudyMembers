c5f52c890b34440748807b8cbe14d778294be826
Oliver03710 <jhee2619@gmail.com>
2022-11-24 15:06:26 +0900

    Clear Code Lines Unused

--- RecruitsStudyMembers/AppDelegate.swift
+++ RecruitsStudyMembers/AppDelegate.swift
@@ -40,7 +40,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         guard let token = Messaging.messaging().fcmToken else { return true }
         print(token)
         UserDefaultsManager.fcmToken = token
-        print("ID Token: \(UserDefaultsManager.token)")
         
         return true
     }
