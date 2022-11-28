d4f1b92b74c7c12af634fb46fbf510c53db3d7b3
Oliver03710 <jhee2619@gmail.com>
2022-11-28 20:15:52 +0900

    Create Button Enumeration To Combine Actions

---
+++ RecruitsStudyMembers/Utilities/Enum/ButtonCombined.swift
@@ -0,0 +1,12 @@
+//
+//  ButtonCombined.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/28.
+//
+
+import Foundation
+
+enum ButtonCombined {
+    case action1, action2
+}
