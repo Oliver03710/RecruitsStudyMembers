154da822779ba6d05af6b6cfde880349f5903fcc
Oliver03710 <jhee2619@gmail.com>
2022-11-28 16:42:48 +0900

    Create Study Member List View Controller

---
+++ RecruitsStudyMembers/Scene/Main/Home/StudyMemberList/MemberListView.swift
@@ -0,0 +1,20 @@
+//
+//  MemberListView.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/28.
+//
+
+import UIKit
+
+final class MemberListView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
