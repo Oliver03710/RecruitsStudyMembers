d6338a796b902215f4c23c901192fca0ab9242a5
Oliver03710 <jhee2619@gmail.com>
2022-11-28 20:05:58 +0900

    Create Comments Embedded In Segmented View Controller

---
+++ RecruitsStudyMembers/Scene/Main/Home/StudyMemberList/NestedViewControllers/SharedSegmentedView.swift
@@ -0,0 +1,20 @@
+//
+//  SharedSegmentedView.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/11/28.
+//
+
+import UIKit
+
+class SharedSegmentedView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
