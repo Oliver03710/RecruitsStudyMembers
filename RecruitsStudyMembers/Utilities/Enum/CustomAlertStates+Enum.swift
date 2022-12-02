a73fbc7396f204c06b77226e81e1847efc914a45
Oliver03710 <jhee2619@gmail.com>
2022-12-02 14:05:52 +0900

    Create Enum To Control Custom Alert View Controller

---
+++ RecruitsStudyMembers/Utilities/Enum/CustomAlertStates+Enum.swift
@@ -0,0 +1,12 @@
+//
+//  CustomAlertStates.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/12/02.
+//
+
+import Foundation
+
+enum CustomAlertState {
+    case deleteAccount, sendRequest, acceptRequest
+}
