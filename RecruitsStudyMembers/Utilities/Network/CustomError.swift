f90933012639a03e1d5b4ccdbc7af971ea1bb58c
Oliver03710 <jhee2619@gmail.com>
2022-12-02 20:40:24 +0900

    Split Case Of Network Status & Error

--- RecruitsStudyMembers/Utilities/Network/CustomError.swift
+++ RecruitsStudyMembers/Utilities/Network/CustomError.swift
@@ -7,50 +7,164 @@
 
 import Foundation
 
-enum SeSacUserError: Int, Error {
-    case alreadySignedup = 201
-    case invalidNickname = 202
-    case firebaseTokenError = 401
-    case unsignedupUser = 406
-    case ServerError = 500
-    case ClientError = 501
+protocol NetworkStatus: Error {
+    
 }
 
-extension SeSacUserError: LocalizedError {
+enum SesacStatus {
+    
+    enum User {
+        enum SignUpSuccess: Int, NetworkStatus {
+            case success = 200
+            case existUser = 201
+            case invalidNickname = 202
+        }
+    }
+    
+    enum Queue {
+        enum FindRequest: Int, NetworkStatus {
+            case success = 200
+            case reportedOver3Times = 201
+            case studyCancelPanaltyLv1 = 203
+            case studyCancelPanaltyLv2 = 204
+            case studyCancelPanaltyLv3 = 205
+        }
+        
+        enum CancelFindRequest: Int, NetworkStatus {
+            case success = 200
+            case requestCanceled = 201
+        }
+        
+        enum myQueueState: Int, NetworkStatus {
+            case success = 200
+            case defaultState = 201
+        }
+        
+        enum StudyRequest: Int, NetworkStatus {
+            case success = 200
+            case alreadyReceivedRequest = 201
+            case userCanceledSeeking = 202
+        }
+        
+        enum AcceptStudyRequest: Int, NetworkStatus {
+            case success = 200
+            case userAlreadyMatched = 201
+            case userCanceledSeeking = 202
+            case meAlreadyMatched = 203
+        }
+        
+        enum Dodge: Int, NetworkStatus {
+            case success = 200
+            case wrongUid = 201
+        }
+    }
+    
+    enum DefaultSuccess: Int, NetworkStatus {
+        case success = 200
+    }
+
+    enum DefaultError: Int, NetworkStatus {
+        case firebase = 401
+        case unsignedUp = 406
+        case server = 500
+        case client = 501
+    }
+}
+
+
+// MARK: - Extension: Default Error Descriptions
+
+extension SesacStatus.DefaultError: LocalizedError {
     
     var errorDescription: String? {
         switch self {
-        case .alreadySignedup: return "이미 가입되어 있는 유저입니다."
+        case .firebase: return "파이어베이스 토큰 만료."
+        case .unsignedUp: return "미가입 유저입니다. 회원가입 화면으로 이동합니다."
+        case .server: return "서버에 문제가 있으니 잠시 후 다시 요청바랍니다."
+        case .client: return "잘못된 요청입니다."
+        }
+    }
+}
+
+// MARK: - Extension: User Success States Descriptions
+
+extension SesacStatus.User.SignUpSuccess: LocalizedError {
+    
+    var errorDescription: String? {
+        switch self {
+        case .success: return "성공"
+        case .existUser: return "이미 가입된 유저입니다."
         case .invalidNickname: return "사용할 수 없는 닉네임입니다. 닉네임을 다시 생성해주세요."
-        case .firebaseTokenError: return "토큰이 만료되었습니다. 토큰을 재발급 받아주세요."
-        case .unsignedupUser: return "미가입된 유저입니다. 회원가입 창으로 이동합니다."
-        case .ServerError: return "서버 에러"
-        case .ClientError: return "클라이언트 에러"
         }
     }
 }
 
-enum SeSacQueueError: Int, Error {
-    case error201 = 201
-    case error202 = 202
-    case error203 = 203
-    case error204 = 204
-    case error205 = 205
-    case firebaseTokenError = 401
-    case unsignedupUser = 406
-    case ServerError = 500
-    case ClientError = 501
-}
+// MARK: - Extension: Queue Success States Descriptions
 
-extension SeSacQueueError: LocalizedError {
+extension SesacStatus.Queue.FindRequest: LocalizedError {
     
     var errorDescription: String? {
         switch self {
-        case .firebaseTokenError: return "토큰이 만료되었습니다. 토큰을 재발급 받아주세요."
-        case .unsignedupUser: return "미가입된 유저입니다. 회원가입 창으로 이동합니다."
-        case .ServerError: return "서버 에러"
-        case .ClientError: return "클라이언트 에러"
-        default: return "에러 발생"
+        case .success: return "성공"
+        case .reportedOver3Times: return "신고를 세번 이상 당한 유저입니다."
+        case .studyCancelPanaltyLv1: return "스터디 취소 패널티 1단계입니다."
+        case .studyCancelPanaltyLv2: return "스터디 취소 패널티 2단계입니다."
+        case .studyCancelPanaltyLv3: return "스터디 취소 패널티 3단계입니다."
         }
     }
 }
+
+extension SesacStatus.Queue.CancelFindRequest: LocalizedError {
+    
+    var errorDescription: String? {
+        switch self {
+        case .success: return "성공"
+        case .requestCanceled: return "이미 매칭된 상태"
+        }
+    }
+}
+
+extension SesacStatus.Queue.myQueueState: LocalizedError {
+    
+    var errorDescription: String? {
+        switch self {
+        case .success: return "성공"
+        case .defaultState: return "일반 상태"
+        }
+    }
+}
+
+extension SesacStatus.Queue.StudyRequest: LocalizedError {
+    
+    var errorDescription: String? {
+        switch self {
+        case .success: return "성공"
+        case .alreadyReceivedRequest: return "이미 요청을 받았습니다."
+        case .userCanceledSeeking: return "상대방이 새싹 찾기를 중단한 상태입니다."
+        }
+    }
+}
+
+extension SesacStatus.Queue.AcceptStudyRequest: LocalizedError {
+    
+    var errorDescription: String? {
+        switch self {
+        case .success: return "성공"
+        case .userAlreadyMatched: return "상대방이 이미 매칭된 상태입니다."
+        case .userCanceledSeeking: return "상대방이 새싹 찾기를 중단한 상태입니다."
+        case .meAlreadyMatched: return "이미 다른 유저와 매칭된 상태입니다."
+        }
+    }
+}
+
+extension SesacStatus.Queue.Dodge: LocalizedError {
+    
+    var errorDescription: String? {
+        switch self {
+        case .success: return "성공"
+        case .wrongUid: return "잘못된 UID 요청"
+        }
+    }
+}
+
+
