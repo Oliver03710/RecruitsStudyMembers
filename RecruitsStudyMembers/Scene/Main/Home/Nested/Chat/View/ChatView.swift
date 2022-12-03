33c8812c0e0e36ef53977e35c073eee9b310484e
Oliver03710 <jhee2619@gmail.com>
2022-12-04 00:05:18 +0900

    Create Chat View Components

---
+++ RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/View/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/12/04.
+//
+
+import UIKit
+
+class ChatView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/ViewModel/ChatViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChatViewModel.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/12/04.
+//
+
+import Foundation
