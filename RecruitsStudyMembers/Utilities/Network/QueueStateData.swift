953d1f083518c07b24d3d8f7a864a563ebe5ac0c
Oliver03710 <jhee2619@gmail.com>
2022-12-03 22:57:50 +0900

    Create Queue State DTO

---
+++ RecruitsStudyMembers/Utilities/Network/QueueStateData.swift
@@ -0,0 +1,13 @@
+//
+//  QueueStateData.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/12/03.
+//
+
+import Foundation
+
+struct QueueStateData: Codable {
+    let reviewed, matched, dodged: Int
+    let matchedNick, matchedUid: String
+}
