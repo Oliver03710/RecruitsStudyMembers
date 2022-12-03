d8224885105e20d0395cae400185ecac342b59b1
Oliver03710 <jhee2619@gmail.com>
2022-12-04 00:46:28 +0900

    Set Initial View Controller For Setting Chat View UI

--- RecruitsStudyMembers/SceneDelegate.swift
+++ RecruitsStudyMembers/SceneDelegate.swift
@@ -17,8 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
-        let vc = SplashViewController()
-        window?.rootViewController = vc
+        let vc = ChatViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        window?.rootViewController = nav
         window?.makeKeyAndVisible()
         
     }
