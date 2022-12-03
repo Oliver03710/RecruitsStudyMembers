4cce85b8dfe8eb23e1ee5e7ef1067c9115cb5032
Oliver03710 <jhee2619@gmail.com>
2022-12-03 20:27:41 +0900

    Create Enum For Queue States

---
+++ RecruitsStudyMembers/Utilities/Enum/QueueStates.swift
@@ -0,0 +1,12 @@
+//
+//  QueueStates.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/12/03.
+//
+
+import Foundation
+
+enum QueueStates {
+    case defaultState, readyToBeMatched, matched
+}
