19e847d63e0c8545ac55631de3e31f5af7746119
Oliver03710 <jhee2619@gmail.com>
2022-12-04 15:14:55 +0900

    Create View And Set Components

--- RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/Views/ChatView.swift
+++ RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/Views/ChatView.swift
@@ -7,10 +7,21 @@
 
 import UIKit
 
+import RxCocoa
+import RxDataSources
+import RxSwift
+import SnapKit
+
 final class ChatView: BaseView {
 
     // MARK: - Properties
     
+    private let tableView: UITableView = {
+        let tv = UITableView()
+        return tv
+    }()
+    
+    private let viewModel = ChatViewModel()
     
     
     // MARK: - Init
@@ -22,8 +33,41 @@ final class ChatView: BaseView {
     
     // MARK: - Helper Functions
     
+    override func configureUI() {
+        bindData()
+    }
+    
     override func setConstraints() {
+        addSubview(tableView)
         
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func bindData() {
+        let cities = ["London", "Vienna", "Lisbon"]
+        
+        let configureCell: (TableViewSectionedDataSource<SectionModel<String, String>>, UITableView,IndexPath, String) -> UITableViewCell = { (datasource, tableView, indexPath,  element) in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ChatTableViewCell.reuseIdentifier, for: indexPath) as? ChatTableViewCell else { return UITableViewCell() }
+            cell.configureCells(text: element)
+            return cell
+        }
+        
+        let datasource = RxTableViewSectionedReloadDataSource<SectionModel<String, String>>.init(configureCell: configureCell)
+        
+        datasource.titleForHeaderInSection = { datasource, index in
+            return datasource.sectionModels[index].model
+        }
+        
+        let sections = [
+            SectionModel<String, String>(model: "first section", items: cities),
+            SectionModel<String, String>(model: "second section", items: cities)
+        ]
+        
+        Observable.just(sections)
+            .bind(to: tableView.rx.items(dataSource: datasource))
+            .disposed(by: viewModel.disposeBag)
     }
 
 }
