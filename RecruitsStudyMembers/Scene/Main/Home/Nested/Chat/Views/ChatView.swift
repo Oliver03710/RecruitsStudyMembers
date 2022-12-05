fb0d92fd94a7784ef1dd382cf2e3d6f1dd87c12e
Oliver03710 <jhee2619@gmail.com>
2022-12-05 12:16:51 +0900

    Add Sections For Date / Add Method To Move Chat View Up When Keyboard Shows / Set Logic To Show Placeholder In Text View

--- RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/Views/ChatView.swift
+++ RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/Views/ChatView.swift
@@ -26,6 +26,10 @@ final class ChatView: BaseView {
     
     private lazy var dataSource = RxTableViewSectionedReloadDataSource<ChatSections> { dataSource, tableView, indexPath, item in
         switch item {
+        case let .introCell(introModel):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ChatTableViewCell.reuseIdentifier, for: indexPath) as? ChatTableViewCell else { return UITableViewCell() }
+            cell.configureCells(text: introModel.string)
+                return cell
         case let .chatCell(cellModel):
             guard let cell = tableView.dequeueReusableCell(withIdentifier: ChatTableViewCell.reuseIdentifier, for: indexPath) as? ChatTableViewCell else { return UITableViewCell() }
             cell.configureCells(text: cellModel.string)
@@ -44,6 +48,9 @@ final class ChatView: BaseView {
     private let textView: UITextView = {
         let tv = UITextView()
         tv.isScrollEnabled = false
+        tv.textColor = SSColors.gray7.color
+        tv.text = "메세지를 입력하세요"
+        tv.font = UIFont(name: SSFonts.body3R14.fonts, size: SSFonts.body3R14.size)
         return tv
     }()
     
@@ -54,6 +61,7 @@ final class ChatView: BaseView {
     }()
     
     private var textViewHeightConstraint: Constraint?
+    var chatViewBottomConstraint: Constraint?
     private let viewModel = ChatViewModel()
     
     
@@ -62,13 +70,29 @@ final class ChatView: BaseView {
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-        
     
+    
+    // MARK: - Selectors
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+//        if textView.isEditable {
+            guard let constraint = chatViewBottomConstraint else { return }
+            moveViewWithKeyboard(notification: notification, viewBottomConstraint: constraint, keyboardWillShow: true)
+//        }
+    }
+        
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        guard let constraint = chatViewBottomConstraint else { return }
+        moveViewWithKeyboard(notification: notification, viewBottomConstraint: constraint, keyboardWillShow: false)
+    }
+
+
     // MARK: - Helper Functions
     
     override func configureUI() {
         addData()
         bindData()
+        keyboardActions()
     }
     
     override func setConstraints() {
@@ -83,8 +107,9 @@ final class ChatView: BaseView {
         }
         
         chatView.snp.makeConstraints {
-            $0.directionalHorizontalEdges.bottom.equalTo(safeAreaLayoutGuide).inset(16)
-            $0.height.lessThanOrEqualTo(88)
+            $0.directionalHorizontalEdges.equalTo(safeAreaLayoutGuide).inset(16)
+            chatViewBottomConstraint = $0.bottom.equalTo(safeAreaLayoutGuide).offset(-16).constraint
+            $0.height.lessThanOrEqualTo(98)
             textViewHeightConstraint = $0.height.greaterThanOrEqualTo(self.textView.contentSize.height + 52).constraint
         }
         
@@ -103,7 +128,14 @@ final class ChatView: BaseView {
     }
     
     private func bindData() {
-        viewModel.usersChat
+        dataSource.titleForHeaderInSection = { dataSource, index in
+            return dataSource.sectionModels[index].header
+        }
+        
+        dataSource.canEditRowAtIndexPath = { dataSource, indexPath in true }
+        dataSource.canMoveRowAtIndexPath = { dataSource, indexPath in true }
+        
+        viewModel.dateSection
             .bind(to: tableView.rx.items(dataSource: dataSource))
             .disposed(by: viewModel.disposeBag)
         
@@ -114,11 +146,17 @@ final class ChatView: BaseView {
             guard let self = self else { return }
             switch actions {
             case .editingDidBegin:
-                print(self.textView.contentSize.height)
+                if self.textView.text == "메세지를 입력하세요" && self.textView.textColor == SSColors.gray7.color {
+                    self.textView.textColor = SSColors.black.color
+                    self.textView.text = ""
+                }
                 self.didUpdateTextViewContentSize()
                 
             case .editingDidEnd:
-                print(self.textView.contentSize.height)
+                if self.textView.text.isEmpty {
+                    self.textView.text = "메세지를 입력하세요"
+                    self.textView.textColor = SSColors.gray7.color
+                }
             }
         }
         .disposed(by: viewModel.disposeBag)
@@ -129,7 +167,7 @@ final class ChatView: BaseView {
                 guard let self = self else { return }
                 let size = CGSize(width: self.textView.frame.width, height: .infinity)
                 let estimatedSize = self.textView.sizeThatFits(size)
-                let isMaxHeight = estimatedSize.height >= 54
+                let isMaxHeight = estimatedSize.height >= 74
                 
                 guard isMaxHeight != self.textView.isScrollEnabled else { return }
                 self.textView.isScrollEnabled = isMaxHeight
@@ -143,7 +181,7 @@ final class ChatView: BaseView {
             .asDriver()
             .drive { [weak self] str in
                 guard let self = self else { return }
-                self.sendButton.setImage(UIImage(named: str.isEmpty ? GeneralIcons.sendInact.rawValue : GeneralIcons.sendAct.rawValue), for: .normal)
+                self.sendButton.setImage(UIImage(named: str.isEmpty || self.textView.textColor == SSColors.gray7.color ? GeneralIcons.sendInact.rawValue : GeneralIcons.sendAct.rawValue), for: .normal)
             }
             .disposed(by: viewModel.disposeBag)
     }
@@ -154,16 +192,45 @@ final class ChatView: BaseView {
         let item3 = ChatItems.chatCell(ChatCellModel(string: "세번째"))
         let item4 = ChatItems.chatCell(ChatCellModel(string: "네번째"))
         let item5 = ChatItems.chatCell(ChatCellModel(string: "다섯번째"))
-        let section1 = ChatSections(items: [item1, item2, item3, item4, item5])
         
-        viewModel.usersChat.accept([section1])
+        let sections = [ChatSections(header: "어제", items: [item1, item2, item3]),
+                                     ChatSections(header: "오늘", items: [item4, item5])]
+        
+        viewModel.dateSection.accept(sections)
     }
     
     private func didUpdateTextViewContentSize() {
-        self.textViewHeightConstraint?.update(offset: self.textView.contentSize.height + 28)
+        textViewHeightConstraint?.update(offset: SSFonts.body3R14.size)
         UIView.animate(withDuration: 0.3) {
             self.layoutIfNeeded()
         }
     }
-
+    
+    private func keyboardActions() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func moveViewWithKeyboard(notification: NSNotification, viewBottomConstraint: Constraint, keyboardWillShow: Bool) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        let keyboardHeight = keyboardSize.height
+        
+        let keyboardDuration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        
+        let keyboardCurve = UIView.AnimationCurve(rawValue: notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! Int)!
+        
+        if keyboardWillShow {
+            let bottomConstant: CGFloat = 16
+            viewBottomConstraint.update(offset: -keyboardHeight + bottomConstant)
+            
+        }else {
+            viewBottomConstraint.update(offset: -16)
+        }
+        
+        let animator = UIViewPropertyAnimator(duration: keyboardDuration, curve: keyboardCurve) { [weak self] in
+            self?.layoutIfNeeded()
+        }
+        
+        animator.startAnimation()
+    }
 }
