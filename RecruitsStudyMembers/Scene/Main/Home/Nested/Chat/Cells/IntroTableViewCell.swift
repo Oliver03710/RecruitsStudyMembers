f78ec30c699b0cb57d6280e397be584cb114e1ce
Oliver03710 <jhee2619@gmail.com>
2022-12-06 02:11:06 +0900

    Set Constraints Its Components

--- RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/Cells/IntroTableViewCell.swift
+++ RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/Cells/IntroTableViewCell.swift
@@ -21,8 +21,17 @@ final class IntroTableViewCell: UITableViewCell {
     
     private let mainLabel: UILabel = {
         let label = UILabel()
-        label.textColor = SSColors.white.color
-        label.font = UIFont(name: SSFonts.title5M12.fonts, size: SSFonts.title5M12.size)
+        label.textColor = SSColors.gray7.color
+        label.font = UIFont(name: SSFonts.title3M14.fonts, size: SSFonts.title3M14.size)
+        return label
+    }()
+    
+    private let subLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = SSColors.gray6.color
+        label.textAlignment = .center
+        label.font = UIFont(name: SSFonts.title4R14.fonts, size: SSFonts.title4R14.size)
+        label.text = "채팅을 통해 약속을 정해보세요 :)"
         return label
     }()
     
@@ -42,9 +51,30 @@ final class IntroTableViewCell: UITableViewCell {
     // MARK: - Helper Functions
     
     private func setConstraints() {
+        contentView.addSubview(bellImageView)
+        contentView.addSubview(mainLabel)
+        contentView.addSubview(subLabel)
+        
+        subLabel.snp.makeConstraints {
+            $0.directionalHorizontalEdges.bottom.equalToSuperview().inset(8)
+            $0.height.equalTo(24)
+        }
+        
+        bellImageView.snp.makeConstraints {
+            $0.centerY.equalTo(mainLabel)
+            $0.width.height.equalTo(16)
+            $0.trailing.equalTo(mainLabel.snp.leading).offset(-4)
+        }
+        
+        mainLabel.snp.makeConstraints {
+            $0.bottom.equalTo(subLabel.snp.top).offset(-4)
+            $0.top.equalToSuperview().inset(8)
+            $0.width.greaterThanOrEqualTo(mainLabel.snp.height)
+            $0.centerX.equalToSuperview().multipliedBy(1.04)
+        }
     }
     
-    func configureCells() {
-        
+    func configureCells(text: String) {
+        mainLabel.text = "\(text)님과 매칭되었습니다"
     }
 }
