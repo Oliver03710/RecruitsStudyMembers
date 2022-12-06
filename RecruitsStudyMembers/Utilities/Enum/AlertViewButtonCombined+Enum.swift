bf0b8c3d2f028bfbaf2ad556f256d364b6ede622
Oliver03710 <jhee2619@gmail.com>
2022-12-07 03:12:23 +0900

    Create Enum For Combine Buttons

---
+++ RecruitsStudyMembers/Utilities/Enum/AlertViewButtonCombined+Enum.swift
@@ -0,0 +1,12 @@
+//
+//  AlertViewButtonCombined+Enum.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/12/06.
+//
+
+import Foundation
+
+enum AlertViewButtonCombined {
+    case illigalOrManner, unpleasantOrAppointment, noShowOrResponse, sexualOrKind, harrasmentOrSkilled, etcOrGoodtime
+}
