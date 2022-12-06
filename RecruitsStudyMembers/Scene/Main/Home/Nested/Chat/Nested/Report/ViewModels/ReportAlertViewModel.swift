20189980408c94f61fe1adaf8e1aa70aba5f4b7b
Oliver03710 <jhee2619@gmail.com>
2022-12-07 03:17:24 +0900

    Put Values Into Input

--- RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/Nested/Report/ViewModels/ReportAlertViewModel.swift
+++ RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/Nested/Report/ViewModels/ReportAlertViewModel.swift
@@ -20,16 +20,37 @@ final class ReportAlertViewModel: CommonViewModel {
     // MARK: - In & Out Data
     
     struct Input {
+        let xmarkButtonTapped: ControlEvent<Void>
+        
+        let illigalOrMannerButtonTapped: ControlEvent<Void>
+        let unpleasantOrAppointmentButtonTapped: ControlEvent<Void>
+        let noShowOrResponseButtonTapped: ControlEvent<Void>
+        let sexualOrKindButtonTapped: ControlEvent<Void>
+        let harrasmentOrSkilledButtonTapped: ControlEvent<Void>
+        let etcOrGoodtimeButtonTapped: ControlEvent<Void>
     }
     
     struct Output {
+        let xmarkButtonDriver: SharedSequence<DriverSharingStrategy, Void>
+        let alertViewButtonMergedDriver: SharedSequence<DriverSharingStrategy, AlertViewButtonCombined>
     }
     
     
     // MARK: - Helper Functions
     
     func transform(input: Input) -> Output {
+        
+        let xmarkButtonDriver = input.xmarkButtonTapped.asDriver()
+        
+        let alertViewButtonMergedDriver = Observable.merge(input.illigalOrMannerButtonTapped.map { AlertViewButtonCombined.illigalOrManner },
+                         input.unpleasantOrAppointmentButtonTapped.map { AlertViewButtonCombined.unpleasantOrAppointment },
+                         input.noShowOrResponseButtonTapped.map { AlertViewButtonCombined.noShowOrResponse },
+                         input.sexualOrKindButtonTapped.map { AlertViewButtonCombined.sexualOrKind },
+                         input.harrasmentOrSkilledButtonTapped.map { AlertViewButtonCombined.harrasmentOrSkilled },
+                         input.etcOrGoodtimeButtonTapped.map { AlertViewButtonCombined.etcOrGoodtime })
+        .asDriver(onErrorJustReturn: .illigalOrManner)
        
-        return Output()
+        return Output(xmarkButtonDriver: xmarkButtonDriver,
+                      alertViewButtonMergedDriver: alertViewButtonMergedDriver)
     }
 }
