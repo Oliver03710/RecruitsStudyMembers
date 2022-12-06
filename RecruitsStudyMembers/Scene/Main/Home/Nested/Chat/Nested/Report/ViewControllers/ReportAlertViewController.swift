ca0e66dd2f396cdf538b451195ba7336e92ed508
Oliver03710 <jhee2619@gmail.com>
2022-12-07 03:13:39 +0900

    Create Button Action With Rx

--- RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/Nested/Report/ViewControllers/ReportAlertViewController.swift
+++ RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/Nested/Report/ViewControllers/ReportAlertViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+
 final class ReportAlertViewController: BaseViewController {
 
     // MARK: - Properties
@@ -27,5 +30,87 @@ final class ReportAlertViewController: BaseViewController {
     
     // MARK: - Helper Functions
     
+    override func configureUI() {
+        binds()
+    }
     
+    private func binds() {
+        let input = ReportAlertViewModel.Input(xmarkButtonTapped: alertView.alertTitleView.xmarkButton.rx.tap,
+                                               illigalOrMannerButtonTapped: alertView.alertBodyView.illigalOrMannerButton.rx.tap,
+                                               unpleasantOrAppointmentButtonTapped: alertView.alertBodyView.unpleasantOrAppointmentButton.rx.tap,
+                                               noShowOrResponseButtonTapped: alertView.alertBodyView.noShowOrResponseButton.rx.tap,
+                                               sexualOrKindButtonTapped: alertView.alertBodyView.sexualOrKindButton.rx.tap,
+                                               harrasmentOrSkilledButtonTapped: alertView.alertBodyView.harrasmentOrSkilledButton.rx.tap,
+                                               etcOrGoodtimeButtonTapped: alertView.alertBodyView.etcOrGoodtimeButton.rx.tap)
+        let output = alertView.viewModel.transform(input: input)
+        
+        output.xmarkButtonDriver
+            .drive { [weak self] _ in
+                self?.dismiss(animated: true)
+            }
+            .disposed(by: alertView.viewModel.disposeBag)
+        
+        output.alertViewButtonMergedDriver
+            .drive { [weak self] actions in
+                guard let self = self else { return }
+                
+                switch actions {
+                case .illigalOrManner:
+                    self.alertView.alertBodyView.illigalOrMannerButton.backgroundColor =
+                    self.alertView.alertBodyView.illigalOrMannerButton.toggle == .off ? SSColors.white.color : SSColors.green.color
+                    self.alertView.alertBodyView.illigalOrMannerButton.layer.borderColor =
+                    self.alertView.alertBodyView.illigalOrMannerButton.toggle == .off ? SSColors.gray4.color.cgColor : SSColors.green.color.cgColor
+                    self.alertView.alertBodyView.illigalOrMannerButton.setTitleColor(self.alertView.alertBodyView.illigalOrMannerButton.toggle == .off ? SSColors.black.color : SSColors.white.color, for: .normal)
+                    self.alertView.alertBodyView.illigalOrMannerButton.tintColor = self.alertView.alertBodyView.illigalOrMannerButton.toggle == .off ? SSColors.black.color : SSColors.white.color
+                    self.alertView.alertBodyView.illigalOrMannerButton.toggle = self.alertView.alertBodyView.illigalOrMannerButton.toggle == .on ? .off : .on
+                    
+                case .unpleasantOrAppointment:
+                    self.alertView.alertBodyView.unpleasantOrAppointmentButton.backgroundColor =
+                    self.alertView.alertBodyView.unpleasantOrAppointmentButton.toggle == .off ? SSColors.white.color : SSColors.green.color
+                    self.alertView.alertBodyView.unpleasantOrAppointmentButton.layer.borderColor =
+                    self.alertView.alertBodyView.unpleasantOrAppointmentButton.toggle == .off ? SSColors.gray4.color.cgColor : SSColors.green.color.cgColor
+                    self.alertView.alertBodyView.unpleasantOrAppointmentButton.setTitleColor(self.alertView.alertBodyView.unpleasantOrAppointmentButton.toggle == .off ? SSColors.black.color : SSColors.white.color, for: .normal)
+                    self.alertView.alertBodyView.unpleasantOrAppointmentButton.tintColor = self.alertView.alertBodyView.unpleasantOrAppointmentButton.toggle == .off ? SSColors.black.color : SSColors.white.color
+                    self.alertView.alertBodyView.unpleasantOrAppointmentButton.toggle = self.alertView.alertBodyView.unpleasantOrAppointmentButton.toggle == .on ? .off : .on
+                    
+                case .noShowOrResponse:
+                    self.alertView.alertBodyView.noShowOrResponseButton.backgroundColor =
+                    self.alertView.alertBodyView.noShowOrResponseButton.toggle == .off ? SSColors.white.color : SSColors.green.color
+                    self.alertView.alertBodyView.noShowOrResponseButton.layer.borderColor =
+                    self.alertView.alertBodyView.noShowOrResponseButton.toggle == .off ? SSColors.gray4.color.cgColor : SSColors.green.color.cgColor
+                    self.alertView.alertBodyView.noShowOrResponseButton.setTitleColor(self.alertView.alertBodyView.noShowOrResponseButton.toggle == .off ? SSColors.black.color : SSColors.white.color, for: .normal)
+                    self.alertView.alertBodyView.noShowOrResponseButton.tintColor = self.alertView.alertBodyView.noShowOrResponseButton.toggle == .off ? SSColors.black.color : SSColors.white.color
+                    self.alertView.alertBodyView.noShowOrResponseButton.toggle = self.alertView.alertBodyView.noShowOrResponseButton.toggle == .on ? .off : .on
+                    
+                case .sexualOrKind:
+                    self.alertView.alertBodyView.sexualOrKindButton.backgroundColor =
+                    self.alertView.alertBodyView.sexualOrKindButton.toggle == .off ? SSColors.white.color : SSColors.green.color
+                    self.alertView.alertBodyView.sexualOrKindButton.layer.borderColor =
+                    self.alertView.alertBodyView.sexualOrKindButton.toggle == .off ? SSColors.gray4.color.cgColor : SSColors.green.color.cgColor
+                    self.alertView.alertBodyView.sexualOrKindButton.setTitleColor(self.alertView.alertBodyView.sexualOrKindButton.toggle == .off ? SSColors.black.color : SSColors.white.color, for: .normal)
+                    self.alertView.alertBodyView.sexualOrKindButton.tintColor = self.alertView.alertBodyView.sexualOrKindButton.toggle == .off ? SSColors.black.color : SSColors.white.color
+                    self.alertView.alertBodyView.sexualOrKindButton.toggle = self.alertView.alertBodyView.sexualOrKindButton.toggle == .on ? .off : .on
+                    
+                case .harrasmentOrSkilled:
+                    self.alertView.alertBodyView.harrasmentOrSkilledButton.backgroundColor =
+                    self.alertView.alertBodyView.harrasmentOrSkilledButton.toggle == .off ? SSColors.white.color : SSColors.green.color
+                    self.alertView.alertBodyView.harrasmentOrSkilledButton.layer.borderColor =
+                    self.alertView.alertBodyView.harrasmentOrSkilledButton.toggle == .off ? SSColors.gray4.color.cgColor : SSColors.green.color.cgColor
+                    self.alertView.alertBodyView.harrasmentOrSkilledButton.setTitleColor(self.alertView.alertBodyView.harrasmentOrSkilledButton.toggle == .off ? SSColors.black.color : SSColors.white.color, for: .normal)
+                    self.alertView.alertBodyView.harrasmentOrSkilledButton.tintColor = self.alertView.alertBodyView.harrasmentOrSkilledButton.toggle == .off ? SSColors.black.color : SSColors.white.color
+                    self.alertView.alertBodyView.harrasmentOrSkilledButton.toggle = self.alertView.alertBodyView.harrasmentOrSkilledButton.toggle == .on ? .off : .on
+                    
+                case .etcOrGoodtime:
+                    self.alertView.alertBodyView.etcOrGoodtimeButton.backgroundColor =
+                    self.alertView.alertBodyView.etcOrGoodtimeButton.toggle == .off ? SSColors.white.color : SSColors.green.color
+                    self.alertView.alertBodyView.etcOrGoodtimeButton.layer.borderColor =
+                    self.alertView.alertBodyView.etcOrGoodtimeButton.toggle == .off ? SSColors.gray4.color.cgColor : SSColors.green.color.cgColor
+                    self.alertView.alertBodyView.etcOrGoodtimeButton.setTitleColor(self.alertView.alertBodyView.etcOrGoodtimeButton.toggle == .off ? SSColors.black.color : SSColors.white.color, for: .normal)
+                    self.alertView.alertBodyView.etcOrGoodtimeButton.tintColor = self.alertView.alertBodyView.etcOrGoodtimeButton.toggle == .off ? SSColors.black.color : SSColors.white.color
+                    self.alertView.alertBodyView.etcOrGoodtimeButton.toggle = self.alertView.alertBodyView.etcOrGoodtimeButton.toggle == .on ? .off : .on
+                    
+                }
+            }
+            .disposed(by: alertView.viewModel.disposeBag)
+    }
 }
