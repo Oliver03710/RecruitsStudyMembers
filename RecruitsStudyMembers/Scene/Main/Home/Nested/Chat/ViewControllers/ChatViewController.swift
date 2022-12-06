aab601973005e5c27531fba19ddf13171eb4e4dc
Oliver03710 <jhee2619@gmail.com>
2022-12-06 16:21:58 +0900

    Add Action To Show More Button Tapped

--- RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/ViewControllers/ChatViewController.swift
+++ RecruitsStudyMembers/Scene/Main/Home/Nested/Chat/ViewControllers/ChatViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
+import SnapKit
+
 final class ChatViewController: BaseViewController {
 
     // MARK: - Properties
     
     let chatView = ChatView()
+    var showMores = false
     
     
     // MARK: - Init
@@ -25,6 +28,32 @@ final class ChatViewController: BaseViewController {
     }
     
     
+    // MARK: - Selectors
+    
+    @objc func showMore() {
+        if !showMores {
+            showMores = true
+            chatView.moreViewHeightConstraint?.update(offset: 0)
+            chatView.opaqueView.isHidden = false
+            chatView.moreView.isHidden = false
+            UIView.animate(withDuration: 0.3) { [weak self] in
+                self?.chatView.opaqueView.backgroundColor = UIColor.black.withAlphaComponent(0.45)
+                self?.view.layoutIfNeeded()
+            }
+        } else {
+            showMores = false
+            chatView.moreViewHeightConstraint?.update(offset: -76)
+            UIView.animate(withDuration: 0.3) { [weak self] in
+                self?.chatView.opaqueView.backgroundColor = UIColor.black.withAlphaComponent(0)
+                self?.view.layoutIfNeeded()
+            } completion: { [weak self] _ in
+                self?.chatView.moreView.isHidden = true
+                self?.chatView.opaqueView.isHidden = true
+            }
+        }
+    }
+    
+    
     // MARK: - Helper Functions
     
     override func configureUI() {
@@ -33,9 +62,22 @@ final class ChatViewController: BaseViewController {
     
     override func setNaigations(naviTitle: String? = nil) {
         super.setNaigations(naviTitle: naviTitle)
-        let stopButton = UIBarButtonItem(image: UIImage(named: GeneralIcons.more.rawValue), style: .plain, target: self, action: nil)
+        let stopButton = UIBarButtonItem(image: UIImage(named: GeneralIcons.more.rawValue), style: .plain, target: self, action: #selector(showMore))
         
         self.navigationItem.rightBarButtonItem = stopButton
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if showMores {
+            showMores = false
+            chatView.moreViewHeightConstraint?.update(offset: -76)
+            UIView.animate(withDuration: 0.3) { [weak self] in
+                self?.chatView.opaqueView.backgroundColor = UIColor.black.withAlphaComponent(0)
+                self?.view.layoutIfNeeded()
+            } completion: { [weak self] _ in
+                self?.chatView.moreView.isHidden = true
+                self?.chatView.opaqueView.isHidden = true
+            }
+        }
+    }
 }
