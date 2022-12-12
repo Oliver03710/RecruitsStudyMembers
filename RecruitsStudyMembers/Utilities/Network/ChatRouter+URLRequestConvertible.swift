7e6585e3412235a41a8432d842228ee359caea29
Oliver03710 <jhee2619@gmail.com>
2022-12-12 22:20:59 +0900

    Remove Unused Case For Chat Networking

--- RecruitsStudyMembers/Utilities/Network/ChatRouter+URLRequestConvertible.swift
+++ RecruitsStudyMembers/Utilities/Network/ChatRouter+URLRequestConvertible.swift
@@ -10,7 +10,7 @@ import Foundation
 import Alamofire
 
 enum SeSacApiChat {
-    case chatGet, chatPost, ChatSignUp
+    case chatGet, chatPost
 }
 
 
@@ -30,14 +30,14 @@ extension SeSacApiChat: URLRequestConvertible {
     
     var path: String {
         switch self {
-        case .chatGet, .ChatSignUp: return UserDefaultsManager.chatGet
-        case .chatPost: return UserDefaultsManager.chatSignUp
+        case .chatGet: return UserDefaultsManager.chatGet
+        case .chatPost: return UserDefaultsManager.chatPost + "/\(NetworkManager.shared.uid)"
         }
     }
     
     var method: HTTPMethod {
         switch self {
-        case .chatGet, .ChatSignUp: return .get
+        case .chatGet: return .get
         case .chatPost: return .post
         }
     }
@@ -45,27 +45,20 @@ extension SeSacApiChat: URLRequestConvertible {
     var headers: HTTPHeaders {
         switch self {
         case .chatGet, .chatPost: return ["Content-Type": UserDefaultsManager.jsonContentType,
-                                          "Authorization": "Bearer \(UserDefaultsManager.token)"]
-            
-        case .ChatSignUp: return ["Content-Type": UserDefaultsManager.jsonContentType]
+                                          "idtoken": UserDefaultsManager.token]
         }
     }
     
     var parameters: [String: Any]? {
         switch self {
-        case .chatPost: return ["text": "시험 텍스트"]
-        case .ChatSignUp: return ["username" : "고래밥",
-                                  "password" : "12345678",
-                                  "name" : "고래밥",
-                                  "email" : "jack4@jack.com",
-                                  "url" : "https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"]
+        case .chatPost: return ["chat": NetworkManager.shared.myChat]
         default: return nil
         }
     }
     
     var encoding: ParameterEncoding {
         switch self {
-        case .chatGet, .chatPost, .ChatSignUp: return JSONEncoding.default
+        case .chatGet, .chatPost: return JSONEncoding.default
         }
     }
     
