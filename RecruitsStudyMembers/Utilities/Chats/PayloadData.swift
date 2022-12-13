fe003c2a3b67d01d562fd5f00f882db76d7d4491
Oliver03710 <jhee2619@gmail.com>
2022-12-13 15:45:36 +0900

    Create DTO To Get Chatting List

---
+++ RecruitsStudyMembers/Utilities/Chats/PayloadData.swift
@@ -0,0 +1,12 @@
+//
+//  PayloadData.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/12/13.
+//
+
+import Foundation
+
+struct Payload: Codable {
+    let payload: [ChatRealm]
+}
