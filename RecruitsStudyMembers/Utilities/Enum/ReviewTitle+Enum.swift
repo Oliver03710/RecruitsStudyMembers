81571643443b795c9db180e4e1793fed459a59a9
Oliver03710 <jhee2619@gmail.com>
2022-12-14 00:01:45 +0900

    Create Enum For Save Review

---
+++ RecruitsStudyMembers/Utilities/Enum/ReviewTitle+Enum.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewTitle+Enum.swift
+//  RecruitsStudyMembers
+//
+//  Created by Junhee Yoon on 2022/12/13.
+//
+
+import Foundation
+
+enum Review: Int, CaseIterable {
+    case illigalOrManner
+    case unpleasantOrAppointment
+    case noShowOrResponse
+    case sexualOrKind
+    case harrasmentOrSkilled
+    case etcOrGoodtime
+}
