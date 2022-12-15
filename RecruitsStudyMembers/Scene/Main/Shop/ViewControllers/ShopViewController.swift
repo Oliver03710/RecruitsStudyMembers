145ab68d77906e29a3200e68e6ccb1cec50c8642
Oliver03710 <jhee2619@gmail.com>
2022-12-15 22:07:55 +0900

    Add Networking Method To Check My Purchase State

--- RecruitsStudyMembers/Scene/Main/Shop/ViewControllers/ShopViewController.swift
+++ RecruitsStudyMembers/Scene/Main/Shop/ViewControllers/ShopViewController.swift
@@ -7,17 +7,20 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+
 final class ShopViewController: BaseViewController {
 
     // MARK: - Properties
     
-    private let myView = ShopView()
+    private let shopView = ShopView()
     
     
     // MARK: - Init
     
     override func loadView() {
-        view = myView
+        view = shopView
     }
     
     override func viewDidLoad() {
@@ -28,7 +31,29 @@ final class ShopViewController: BaseViewController {
     // MARK: - Helper Functions
     
     override func configureUI() {
-        
+        checkMyShopState()
     }
     
+    private func checkMyShopState() {
+        NetworkManager.shared.request(ShopData.self, router: SeSacApiShop.checkMyInfo)
+            .subscribe(onSuccess: { response, _ in
+                NetworkManager.shared.shopState = response
+                
+            }, onFailure: { [weak self] error in
+                let err = (error as NSError).code
+                print(err)
+                guard let errStatus = SesacStatus.DefaultError(rawValue: err) else { return }
+                switch errStatus {
+                case .firebase:
+                    NetworkManager.shared.fireBaseError {
+                        self?.checkMyShopState()
+                    } errorHandler: {
+                        self?.view.makeToast("에러가 발생했습니다. 잠시 후 다시 실행해주세요.")
+                    }
+                    
+                default: self?.view.makeToast(errStatus.errorDescription)
+                }
+            })
+            .disposed(by: shopView.viewModel.disposeBag)
+    }
 }
